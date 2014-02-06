7ae7c1e86542e577cc18def0a3d16d3398cf5f81
antonioalcocer <aalcocer@paradigmatecnologico.com>
2014-02-06 17:13:27 +0100

    First tentative for Stop Process

---
+++ src/main/java/com/stratio/sdh/meta/statements/StopProcessStatement.java
@@ -0,0 +1,36 @@
+package com.stratio.sdh.meta.statements;
+
+import com.stratio.sdh.meta.structures.Path;
+
+public class StopProcessStatement extends Statement {
+
+    private String ident;
+
+    public StopProcessStatement(String ident) {
+        this.ident = ident;
+    }    
+    
+    public String getIdent() {
+        return ident;
+    }
+
+    public void setIdent(String ident) {
+        this.ident = ident;
+    }
+
+    
+    @Override
+    public Path estimatePath() {
+        return Path.CASSANDRA;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Stop process ");
+        sb.append(ident);
+        return sb.toString();
+    }
+    
+    
+    
+}
