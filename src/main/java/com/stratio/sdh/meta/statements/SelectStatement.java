84a19c3c3d8d074a178f60c9275c352684ec18e1
Miguel Angel Fernandez Diaz <mafernandez@paradigmatecnologico.com>
2014-02-11 17:10:24 +0100

    Merge before SelectStatement creation

--- src/main/java/com/stratio/sdh/meta/statements/SelectStatement.java
+++ src/main/java/com/stratio/sdh/meta/statements/SelectStatement.java
@@ -1,11 +1,37 @@
 package com.stratio.sdh.meta.statements;
 
+import com.stratio.sdh.meta.structures.Option;
 import com.stratio.sdh.meta.structures.Path;
+import java.util.List;
 
 public class SelectStatement extends Statement {
 
+    private SelectionClause selectionClause;    
+    private String tablename;
+    private WindowSelect window;
+    private InnerJoin join;
+    private boolean whereInc;
+    private WhereClause where;
+    private boolean orderInc;
+    private OrderBy order;
+    private GroupBy group;
+    private boolean limitInc;
+    private int limit;
+    private boolean disableAnalytics;
+    private boolean optsInc;
+    private List<Option> options;
+    
+
     public SelectStatement() {
-    }   
+    } 
+    
+    public String getTablename() {
+        return tablename;
+    }
+
+    public void setTablename(String tablename) {
+        this.tablename = tablename;
+    }          
     
     @Override
     public Path estimatePath() {
