a0e9e36eddfac7f02e1c6349f1691535ac54ae45
Miguel Angel Fernandez Diaz <mafernandez@paradigmatecnologico.com>
2014-02-28 13:30:57 +0100

    Parsing test split into Parsing & Cassandra tests

--- src/test/java/com/stratio/meta/grammar/ParsingTest.java
+++ src/test/java/com/stratio/meta/grammar/ParsingTest.java
@@ -1,17 +1,10 @@
 package com.stratio.meta.grammar;
 
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Statement;
-import com.datastax.driver.core.exceptions.DriverException;
-import com.datastax.driver.core.exceptions.NoHostAvailableException;
-import com.stratio.meta.cassandra.CassandraClient;
 import static org.junit.Assert.*;
 
 import java.util.HashMap;
 
 import org.junit.Test;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 
 import com.stratio.meta.statements.CreateIndexStatement;
 import com.stratio.meta.statements.MetaStatement;
@@ -34,70 +27,15 @@ public class ParsingTest {
 	/**
 	 * Class logger.
 	 */
-	private static final Logger logger = Logger.getLogger(ParsingTest.class);	
-        
-	public static void initCassandraConnection(){
-            try {
-                CassandraClient.connect();
-            } catch(NoHostAvailableException ex){
-                logger.error("\033[31mCannot connect with Cassandra\033[0m", ex);  
-                System.exit(-1);
-            }
-	}
-        
-	public static void checkKeyspaces(){
-            try {
-                CassandraClient.executeQuery("USE testKS", false);
-                logger.error("\033[31mKeyspace \'testKs\' already exists\033[0m");  
-                System.exit(-1);
-            } catch(DriverException ex){
-                logger.info("Creating keyspace \'testKS\' in Cassandra for testing purposes");
-            }
-	}
-        
-        @BeforeClass
-        public static void init(){
-            initCassandraConnection();
-            checkKeyspaces();
-        }
-        
-        public static void dropKeyspaces(){
-            CassandraClient.executeQuery("DROP KEYSPACE IF EXISTS testKS;", false);
-	}
-        
-	public static void closeCassandraConnection(){
-            CassandraClient.close();
-	}
-
-        @AfterClass
-        public static void exit(){
-            dropKeyspaces();
-            closeCassandraConnection();
-        }        	
-                        
+	private static final Logger logger = Logger.getLogger(ParsingTest.class);	                
+                               
         public MetaStatement testRegularStatement(String inputText, String methodName) {
             AntlrResult antlrResult = MetaUtils.parseStatement(inputText);
             MetaStatement st = antlrResult.getStatement();
             assertNotNull("Cannot parse "+methodName, st);             
             assertTrue("Cannot parse "+methodName, inputText.equalsIgnoreCase(st.toString()+";"));
             return st;
-        }
-        
-        public void testDriverStatement(MetaStatement st, String methodName){
-            Statement driverStmt = st.getDriverStatement();
-            ResultSet resultSet;
-            if(driverStmt != null){
-                resultSet = CassandraClient.executeQuery(driverStmt, false);
-            } else {
-                resultSet = CassandraClient.executeQuery(st.translateToCQL(), false);
-            }            
-            assertNotNull("Cannot execute "+methodName+" in Cassandra", resultSet);
-        }
-        
-        public void testStatementWithCassandra(String inputText, String methodName){
-            MetaStatement st = testRegularStatement(inputText, methodName);
-            testDriverStatement(st, methodName);
-        }
+        }         
         
         public void testMetaError(String inputText){
             AntlrResult antlrResult = MetaUtils.parseStatement(inputText);
@@ -110,12 +48,6 @@ public class ParsingTest {
             AntlrResult antlrResult = MetaUtils.parseStatement(inputText);
             MetaStatement st = antlrResult.getStatement();
             assertTrue("No errors reported in "+methodName, antlrResult.getFoundErrors().getNumberOfErrors()>0);
-        }
-        
-        public void testCassandraError(MetaStatement st){
-            Statement driverStmt = st.getDriverStatement();
-            thrown.expect(DriverException.class); 
-            ResultSet resultSet = CassandraClient.executeQuery(driverStmt, false);
         }        	
 
         // CREATE KEYSPACE
@@ -332,7 +264,6 @@ public class ParsingTest {
 	}
 
 	//DROP INDEX
-
 	@Test
 	public void dropIndex_basic() {
 		String inputText = "DROP INDEX index_name;";
@@ -360,7 +291,6 @@ public class ParsingTest {
 	}                
         
 	//ADD
-
 	@Test
 	public void add_basic() {
 		String inputText = "ADD \"jar_name-v1.0.jar\";";
@@ -672,123 +602,6 @@ public class ParsingTest {
             String inputText = "drop trigger trigger1 on table_name;";
             testRegularStatement(inputText, "dropTrigger");
 	}
-
-        // CASSANDRA TESTS
-	public void createKeyspace_basic() {
-            String inputText = "CREATE KEYSPACE testKS WITH replication = {class: SimpleStrategy, replication_factor: 1}"
-                    + " AND durable_writes = false;";		
-            AntlrResult antlrResult = MetaUtils.parseStatement(inputText);
-            MetaStatement st = antlrResult.getStatement();
-            assertNotNull("Cannot parse createKeyspace_basic", st);
-
-            boolean originalOK = false;
-            boolean alternative1 = false;
-
-            if(inputText.equalsIgnoreCase(st.toString()+";")){
-                originalOK = true;
-            }
-
-            String alternative1Str = "CREATE KEYSPACE testKS WITH replication = {replication_factor: 1, class: SimpleStrategy}"
-                    + " AND durable_writes = false;";
-            if(alternative1Str.equalsIgnoreCase(st.toString()+";")){
-                alternative1 = true;
-            }
-
-            assertTrue("Cannot parse createKeyspace_basic", (originalOK || alternative1));    
-            
-            testDriverStatement(st, "createKeyspace_basic");
-	}
-        
-	public void createTable_basic_simple() {
-		String inputText = "CREATE TABLE testKS.users(name varchar, password varchar, color varchar, gender varchar,"
-                        + " food varchar, animal varchar, age int, code int, PRIMARY KEY ((name, gender), color, animal));";
-		testStatementWithCassandra(inputText, "createTable_basic_simple");
-	}
-        
-	public void insert_into_simple_1() {
-            String inputText = "INSERT INTO testKS.users (name, gender, color, animal, food, password, age, code)"
-                    + " VALUES(pepito, male, orange, whale, plants, efg, 12, 44);";
-            testStatementWithCassandra(inputText, "insert_into_simple_1");
-	}
-        
-        public void insert_into_simple_2() {
-            String inputText = "INSERT INTO testKS.users (name, gender, color, animal, food, password, age, code) "
-                    + "VALUES(pepito, male, black, whale, plants, efg, 12, 44) IF NOT EXISTS USING TTL = 86400;"; 
-            testStatementWithCassandra(inputText, "insert_into_simple_2");
-        }
-        
-        public void insert_into_simple_3() {
-            String inputText = "INSERT INTO testKS.users (name, gender, color, animal, food, password, age, code) "
-                    + "VALUES(pepita, female, blue, dog, meat, sdh, 5, 33);"; 
-            testStatementWithCassandra(inputText, "insert_into_simple_3");
-        }
-        
-        public void insert_into_simple_4() {
-            String inputText = "INSERT INTO testKS.users (name, gender, color, animal, food, password, age, code) "
-                    + "VALUES(pepita, female, red, cat, mice, kdf, 3, 22);";
-            testStatementWithCassandra(inputText, "insert_into_simple_4");
-        }
-        
-        public void insert_into_simple_5() {
-            String inputText = "INSERT INTO testKS.users (name, gender, color, animal, food, password, age, code) "
-                    + "VALUES(pepita, female, black, fish, plankton, dfp, 3, 11);"; 
-            testStatementWithCassandra(inputText, "insert_into_simple_5");
-        }
-        
-        public void insert_into_simple_6() {
-            String inputText = "INSERT INTO testKS.users (name, gender, color, animal, food, password, age, code) "
-                    + "VALUES(pepito, male, green, cat, cookies, cas, 5, 55);";
-            testStatementWithCassandra(inputText, "insert_into_simple_6");
-        }
-        
-	public void select_all() {
-            String inputText = "SELECT * FROM testKS.users;";
-            testStatementWithCassandra(inputText, "select_all");
-	}
-        
-	public void select_order() {
-		String inputText = "SELECT name, gender, color, animal AS rage, age, food FROM testKS.users "
-                        + "WHERE name = pepita AND gender = female ORDER BY color DESC LIMIT 2;";
-		testStatementWithCassandra(inputText, "select_order");
-	}
-                        
-	public void select_count() {
-		String inputText = "SELECT COUNT(*) FROM testKS.users;";
-		testStatementWithCassandra(inputText, "select_count");
-	}
-                        
-	public void select_distinct() {
-		String inputText = "SELECT DISTINCT name, gender FROM testKS.users;";
-		testStatementWithCassandra(inputText, "select_distinct");
-	}
-        
-	public void truncate_tableWithKeyspace() {
-		String inputText = "TRUNCATE testKS.users;";
-		testStatementWithCassandra(inputText, "truncate_tableWithKeyspace");
-	}
-        
-	public void dropKeyspace_simple() {
-		String inputText = "drop keyspace IF EXISTS testKS;";
-		testStatementWithCassandra(inputText, "dropKeyspace_simple");
-	}
-        
-        @Test
-        public void test_cassandra_1(){
-            createKeyspace_basic();
-            createTable_basic_simple();
-            insert_into_simple_1();
-            insert_into_simple_2();
-            insert_into_simple_3();
-            insert_into_simple_4(); 
-            insert_into_simple_5();
-            insert_into_simple_6();
-            select_all();
-            select_order();
-            select_count();
-            select_distinct();
-            truncate_tableWithKeyspace();
-            dropKeyspace_simple();
-        }
         
 	// TEST META EXCEPTIONS
 	@Rule
@@ -938,22 +751,6 @@ public class ParsingTest {
 	public void drop_trigger_wrong_assignment(){
 		String inputText = "drop trigger trigger1 on table_name = 20;";
 		testMetaError(inputText);
-	}
-        
-        // TEST CASSANDRA EXCEPTIONS
-	public void insert_into_wrong_data_type(){   
-            String inputText = "INSERT INTO testKS.users (name, gender, color, animal, food, password, age, code)"
-                    + " VALUES(pepito, male, black, whale, plants, efg, twelve, 44);";
-            MetaStatement st = testRegularStatement(inputText, "insert_into_wrong_data_type");
-            testCassandraError(st);                       
-	}
-        
-        @Test
-	public void test_cassandra_wrong_1(){   
-            createKeyspace_basic();
-            createTable_basic_simple();
-            insert_into_wrong_data_type();
-            dropKeyspace_simple();
-	}        
+	}                        
         
 }
