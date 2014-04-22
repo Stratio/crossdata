9364bd20bc7830dec76590b7b63468317fc35eef
Gaspar <gmunoz@paradigmatecnologico.com>
2014-04-22 16:25:37 +0200

    Deep Tests and minor changes in Bridge.java

--- meta-core/src/test/java/com/stratio/meta/core/cassandra/BasicCoreCassandraTest.java
+++ meta-core/src/test/java/com/stratio/meta/core/cassandra/BasicCoreCassandraTest.java
@@ -71,7 +71,6 @@ public class BasicCoreCassandraTest {
 
     @AfterClass
     public static void tearDownAfterClass(){
-
         dropKeyspaceIfExists("testKs");
         closeCassandraConnection();
     }
