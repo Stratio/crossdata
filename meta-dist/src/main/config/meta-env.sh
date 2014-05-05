dc42f42e03a55c3ec7bdd4e408de632a69a9c0e8
dhiguero <dhiguero@stratio.com>
2014-05-05 12:46:45 +0200

    META-HOME is now set by the user and not automatically discovered. (Fix bug when executing as service)

--- meta-dist/src/main/config/meta-env.sh
+++ meta-dist/src/main/config/meta-env.sh
@@ -1,38 +1,23 @@
 #!/bin/bash
-PRG="$0"
-while [ -h "$PRG" ]; do
-  ls=`ls -ld "$PRG"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    PRG="$link"
-  else
-    PRG=`dirname "$PRG"`/"$link"
-  fi
-done
-
-PRGDIR=`dirname "$PRG"`
-BASEDIR=`cd "$PRGDIR/.." >/dev/null; pwd`
-
-
 
 # META_CONF is global enviroment variable
 # JAVA_OPTS
 # JSVCCMD
 # JAVA_HOME
 # META_HOME
-META_HOME="$BASEDIR"
+META_HOME="."
 # META_LIB
-META_LIB="$META_HOME/lib"
+META_LIB="${META_HOME}/lib"
 # META_BIN
-META_BIN="$META_HOME/bin"
+META_BIN="${META_HOME}/bin"
 # META_LOG_OUT
-META_LOG_OUT="$META_HOME/meta-server.out"
+META_LOG_OUT="${META_HOME}/meta-server.out"
 # META_LOG_ERROR
-META_LOG_ERR="$META_HOME/meta-server.err"
+META_LOG_ERR="${META_HOME}/meta-server.err"
 # META_SERVER_USER
 META_SERVER_USER="root"
 # META_SERVER_PID
-META_SERVER_PID="$META_HOME/meta-server.pid"
+META_SERVER_PID="${META_HOME}/meta-server.pid"
 
 echo "META_HOME = $META_HOME"
 echo "META_CONF = $META_CONF"
