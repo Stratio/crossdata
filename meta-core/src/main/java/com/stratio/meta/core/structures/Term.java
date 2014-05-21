6bee0ea535afe89d8142039278e31cfc95786b63
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2014-05-21 15:28:41 +0200

    Equals and hashCode functions implemented for Term

--- meta-core/src/main/java/com/stratio/meta/core/structures/Term.java
+++ meta-core/src/main/java/com/stratio/meta/core/structures/Term.java
@@ -19,10 +19,17 @@
 
 package com.stratio.meta.core.structures;
 
-public abstract class Term extends ValueCell {
+import java.io.Serializable;
+
+public abstract class Term extends ValueCell implements Serializable {
+
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = 5665555235623134239L;
 
     protected Class<?> clazz;
-    protected Object value;
+    //protected Object value;
 
     /**
      * Get the Term Java Class.
@@ -41,5 +48,53 @@ public abstract class Term extends ValueCell {
      * @return A {@link java.lang.Object} with the value.
      */
     public abstract Object getTermValue();
-    
+
+    /**
+     * Returns a hash code value for the object. This method is
+     * supported for the benefit of hash tables such as those provided by
+     * {@link java.util.HashMap}.
+     *
+     * @return a hash code value for this object.
+     * @see Object#equals(Object)
+     * @see System#identityHashCode
+     */
+    @Override
+    public int hashCode() {
+        return clazz.hashCode() * this.getTermValue().hashCode();
+    }
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * <p/>
+     * The {@code equals} method implements an equivalence relation
+     * on non-null object references:
+     *
+     * @param obj the reference object with which to compare.
+     * @return {@code true} if this object is the same as the obj
+     * argument; {@code false} otherwise.
+     * @see #hashCode()
+     * @see java.util.HashMap
+     */
+    @Override
+    public boolean equals(Object obj) {
+
+        if(obj == null){
+            return false;
+        }
+
+        if(!(obj instanceof Term)){
+            return false;
+        }
+
+        Term other = (Term) obj;
+        if(other.getClass() != this.getClass()){
+            return false;
+        }
+
+        if(other.clazz != this.clazz){
+            return false;
+        }
+
+        return other.getTermValue().equals(this.getTermValue());
+    }
 }
