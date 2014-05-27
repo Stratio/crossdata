fbc2239cf1f798ca987b8ad5648d0befb6335b3c
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2014-05-27 17:04:12 +0200

    Minor bugs fixed

--- meta-core/src/main/java/com/stratio/meta/core/executor/StreamExecutor.java
+++ meta-core/src/main/java/com/stratio/meta/core/executor/StreamExecutor.java
@@ -13,35 +13,37 @@ import java.util.List;
 import java.util.Map;
 
 
-/**
- * Created by aalcocer on 5/27/14. To generate unit test of proxy actor
- */
 public class StreamExecutor {
 
-  public static Result execute(MetaStatement stmt) {
+    public static Result execute(MetaStatement stmt) {
 
-      Result result = QueryResult.createSuccessQueryResult();
+        System.out.println("TRACE: StreamExecutor");
 
-      if (stmt instanceof CreateTableStatement) {
-          CreateTableStatement cts= (CreateTableStatement) stmt;
-          String tableEphimeralName= cts.getTableName() ;
-          List<ColumnNameType> columnList = Arrays.asList();
-          for (Map.Entry<String, String> column : cts.getColumns().entrySet()) {
-              ColumnType type=null;
-              if (column.getValue().equalsIgnoreCase("string"))type=ColumnType.STRING;
-              else if (column.getValue().equalsIgnoreCase("boolean"))type=ColumnType.BOOLEAN;
-              else if (column.getValue().equalsIgnoreCase("doble"))type=ColumnType.DOUBLE;
-              else if (column.getValue().equalsIgnoreCase("float"))type=ColumnType.FLOAT;
-              else if (column.getValue().equalsIgnoreCase("integer"))type=ColumnType.INTEGER;
-              else if (column.getValue().equalsIgnoreCase("long"))type=ColumnType.LONG;
-              ColumnNameType StreamColumn= new ColumnNameType(column.getKey(), type);
-              columnList.add(StreamColumn);
-          }
-          return MetaStream.createStream(tableEphimeralName, columnList);
-      } else {
-          return QueryResult.createFailQueryResult("Not sopported yet");
-      }
-  }
+        Result result = QueryResult.createSuccessQueryResult();
+
+        if (stmt instanceof CreateTableStatement) {
+            CreateTableStatement cts= (CreateTableStatement) stmt;
+            String tableEphimeralName= cts.getTableName() ;
+            List<ColumnNameType> columnList = Arrays.asList();
+            for (Map.Entry<String, String> column : cts.getColumns().entrySet()) {
+                System.out.println("Adding column: "+column);
+                ColumnType type=null;
+                if (column.getValue().equalsIgnoreCase("string")) type=ColumnType.STRING;
+                else if (column.getValue().equalsIgnoreCase("boolean")) type=ColumnType.BOOLEAN;
+                else if (column.getValue().equalsIgnoreCase("doble")) type=ColumnType.DOUBLE;
+                else if (column.getValue().equalsIgnoreCase("float")) type=ColumnType.FLOAT;
+                else if (column.getValue().equalsIgnoreCase("integer")) type=ColumnType.INTEGER;
+                else if (column.getValue().equalsIgnoreCase("long")) type=ColumnType.LONG;
+                else type = ColumnType.valueOf(column.getValue());
+                ColumnNameType StreamColumn= new ColumnNameType(column.getKey(), type);
+                columnList.add(StreamColumn);
+            }
+            System.out.println("Creating stream");
+            return MetaStream.createStream(tableEphimeralName, columnList);
+        } else {
+            return QueryResult.createFailQueryResult("Not sopported yet");
+        }
+    }
 
 }
 
