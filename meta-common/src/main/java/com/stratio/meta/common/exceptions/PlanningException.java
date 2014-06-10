38576aef06932c9c03b9bb6a8bb7160580bb9be8
dhiguero <dhiguero@stratio.com>
2014-06-10 18:53:19 +0200

    Adding async driver functionality

---
+++ meta-common/src/main/java/com/stratio/meta/common/exceptions/PlanningException.java
@@ -0,0 +1,8 @@
+package com.stratio.meta.common.exceptions;
+
+/**
+ * Created by dhiguero on 10/06/14.
+ */
+public class PlanningException {
+
+}
---
+++ meta-common/src/main/java/com/stratio/meta/common/exceptions/ValidationException.java
@@ -0,0 +1,8 @@
+package com.stratio.meta.common.exceptions;
+
+/**
+ * Created by dhiguero on 9/06/14.
+ */
+public class ValidationException {
+
+}
