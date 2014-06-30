d376a303d443f75acd76da5c30393ec23036744e
Óscar Puertas <opuertas@paradigmatecnologico.com>
2014-06-30 16:48:34 +0200

    Bug fixed #34: Removing stration internal column names from the metadata

--- meta-core/src/main/java/com/stratio/meta/core/utils/CoreUtils.java
+++ meta-core/src/main/java/com/stratio/meta/core/utils/CoreUtils.java
@@ -1,47 +1,47 @@
 /*
  * Stratio Meta
- *
+ * 
  * Copyright (c) 2014, Stratio, All rights reserved.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library.
  */
 
 package com.stratio.meta.core.utils;
 
-import com.datastax.driver.core.*;
-import com.datastax.driver.core.ColumnMetadata;
-import com.stratio.meta.common.data.CassandraResultSet;
-import com.stratio.meta.common.data.Cell;
-import com.stratio.meta.common.data.ColumnDefinition;
-import com.stratio.meta.common.metadata.structures.*;
-import com.stratio.meta.core.metadata.AbstractMetadataHelper;
-import com.stratio.meta.core.metadata.CassandraMetadataHelper;
-import com.stratio.meta.core.structures.FloatTerm;
-import com.stratio.meta.core.structures.IntegerTerm;
-import com.stratio.meta.core.structures.Term;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.*;
+import com.datastax.driver.core.ColumnDefinitions;
+import com.datastax.driver.core.ColumnMetadata;
+import com.datastax.driver.core.DataType;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.stratio.meta.common.data.CassandraResultSet;
+import com.stratio.meta.common.data.Cell;
+import com.stratio.meta.common.metadata.structures.ColumnType;
+import com.stratio.meta.core.metadata.AbstractMetadataHelper;
+import com.stratio.meta.core.metadata.CassandraMetadataHelper;
+import com.stratio.meta.core.structures.Term;
 
 public class CoreUtils {
 
   public static final List<String> supportedTypes = Arrays.asList("bigint", "boolean", "counter",
-                                                                  "double", "float", "int",
-                                                                  "integer", "varchar");
+      "double", "float", "int", "integer", "varchar");
   // SOON: "date", "uuid", "timeuuid"
 
   /**
@@ -57,35 +57,35 @@ public class CoreUtils {
 
   static {
     try {
-      transformations
-          .put(DataType.ascii().toString(), Row.class.getMethod("getString", String.class));
-      transformations
-          .put(DataType.text().toString(), Row.class.getMethod("getString", String.class));
-      transformations
-          .put(DataType.varchar().toString(), Row.class.getMethod("getString", String.class));
-      transformations
-          .put(DataType.bigint().toString(), Row.class.getMethod("getLong", String.class));
-      transformations
-          .put(DataType.counter().toString(), Row.class.getMethod("getLong", String.class));
-      transformations
-          .put(DataType.cboolean().toString(), Row.class.getMethod("getBool", String.class));
+      transformations.put(DataType.ascii().toString(),
+          Row.class.getMethod("getString", String.class));
+      transformations.put(DataType.text().toString(),
+          Row.class.getMethod("getString", String.class));
+      transformations.put(DataType.varchar().toString(),
+          Row.class.getMethod("getString", String.class));
+      transformations.put(DataType.bigint().toString(),
+          Row.class.getMethod("getLong", String.class));
+      transformations.put(DataType.counter().toString(),
+          Row.class.getMethod("getLong", String.class));
+      transformations.put(DataType.cboolean().toString(),
+          Row.class.getMethod("getBool", String.class));
       transformations
           .put(DataType.blob().toString(), Row.class.getMethod("getBytes", String.class));
-      transformations
-          .put(DataType.decimal().toString(), Row.class.getMethod("getDecimal", String.class));
-      transformations
-          .put(DataType.cdouble().toString(), Row.class.getMethod("getDouble", String.class));
-      transformations
-          .put(DataType.cfloat().toString(), Row.class.getMethod("getFloat", String.class));
+      transformations.put(DataType.decimal().toString(),
+          Row.class.getMethod("getDecimal", String.class));
+      transformations.put(DataType.cdouble().toString(),
+          Row.class.getMethod("getDouble", String.class));
+      transformations.put(DataType.cfloat().toString(),
+          Row.class.getMethod("getFloat", String.class));
       transformations.put(DataType.inet().toString(), Row.class.getMethod("getInet", String.class));
       transformations.put(DataType.cint().toString(), Row.class.getMethod("getInt", String.class));
-      transformations
-          .put(DataType.timestamp().toString(), Row.class.getMethod("getDate", String.class));
+      transformations.put(DataType.timestamp().toString(),
+          Row.class.getMethod("getDate", String.class));
       transformations.put(DataType.uuid().toString(), Row.class.getMethod("getUUID", String.class));
-      transformations
-          .put(DataType.timeuuid().toString(), Row.class.getMethod("getUUID", String.class));
-      transformations
-          .put(DataType.varint().toString(), Row.class.getMethod("getVarint", String.class));
+      transformations.put(DataType.timeuuid().toString(),
+          Row.class.getMethod("getUUID", String.class));
+      transformations.put(DataType.varint().toString(),
+          Row.class.getMethod("getVarint", String.class));
     } catch (NoSuchMethodException e) {
       LOG.error("Cannot create transformation map", e);
     }
@@ -93,25 +93,25 @@ public class CoreUtils {
 
   /**
    * Get a {@link com.stratio.meta.common.data.Cell} with the column contents of a Row.
-   *
-   * @param type       The {@link com.datastax.driver.core.DataType} of the column.
-   * @param r          The row that contains the column.
+   * 
+   * @param type The {@link com.datastax.driver.core.DataType} of the column.
+   * @param r The row that contains the column.
    * @param columnName The column name.
    * @return A {@link com.stratio.meta.common.data.Cell} with the contents.
    * @throws InvocationTargetException If the required method cannot be invoked.
-   * @throws IllegalAccessException    If the method cannot be accessed.
+   * @throws IllegalAccessException If the method cannot be accessed.
    */
-  protected Cell getCell(DataType type, Row r, String columnName)
-      throws InvocationTargetException, IllegalAccessException {
+  protected Cell getCell(DataType type, Row r, String columnName) throws InvocationTargetException,
+      IllegalAccessException {
     Method m = transformations.get(type.toString());
     Object value = m.invoke(r, columnName);
     return new Cell(value);
   }
 
   /**
-   * Transforms a Cassandra {@link com.datastax.driver.core.ResultSet} into a {@link
-   * com.stratio.meta.common.data.ResultSet}.
-   *
+   * Transforms a Cassandra {@link com.datastax.driver.core.ResultSet} into a
+   * {@link com.stratio.meta.common.data.ResultSet}.
+   * 
    * @param resultSet The input Cassandra result set.
    * @return An equivalent Meta ResultSet
    */
@@ -120,15 +120,20 @@ public class CoreUtils {
 
     AbstractMetadataHelper helper = new CassandraMetadataHelper();
 
-    //Get the columns in order
+    // Get the columns in order
     List<ColumnDefinitions.Definition> definitions = resultSet.getColumnDefinitions().asList();
     List<com.stratio.meta.common.metadata.structures.ColumnMetadata> columnList = new ArrayList<>();
     com.stratio.meta.common.metadata.structures.ColumnMetadata columnMetadata = null;
-    //Obtain the metadata associated with the columns.
+    // Obtain the metadata associated with the columns.
     for (ColumnDefinitions.Definition def : definitions) {
+
+      if (def.getName().toLowerCase().startsWith("stratio")) {
+        continue;
+      }
+
       columnMetadata =
           new com.stratio.meta.common.metadata.structures.ColumnMetadata(def.getTable(),
-                                                                         def.getName());
+              def.getName());
       ColumnType type = helper.toColumnType(def.getType().getName().toString());
       columnMetadata.setType(type);
       columnList.add(columnMetadata);
@@ -156,10 +161,8 @@ public class CoreUtils {
 
   public static boolean castForLongType(ColumnMetadata cm, Term<?> term) {
     boolean required = false;
-    if (((cm.getType().asJavaClass() == Integer.class)
-         || (cm.getType().asJavaClass() == Long.class))
-        && ((term.getTermClass() == Integer.class)
-            || (term.getTermClass() == Long.class))) {
+    if (((cm.getType().asJavaClass() == Integer.class) || (cm.getType().asJavaClass() == Long.class))
+        && ((term.getTermClass() == Integer.class) || (term.getTermClass() == Long.class))) {
       required = true;
     }
     return required;
@@ -167,10 +170,8 @@ public class CoreUtils {
 
   public static boolean castForDoubleType(ColumnMetadata cm, Term<?> term) {
     boolean required = false;
-    if ((cm.getType().asJavaClass() == Double.class)
-        || (cm.getType().asJavaClass() == Float.class)
-           && ((term.getTermClass() == Double.class)
-               || (term.getTermClass() == Float.class))) {
+    if ((cm.getType().asJavaClass() == Double.class) || (cm.getType().asJavaClass() == Float.class)
+        && ((term.getTermClass() == Double.class) || (term.getTermClass() == Float.class))) {
       required = true;
     }
     return required;
