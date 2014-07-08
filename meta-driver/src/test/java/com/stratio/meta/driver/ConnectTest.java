9fa2195f46f07f6c15f8e373c623c028fd6e8908
idiazstratio <idiaz@stratio.com>
2014-07-08 09:20:34 +0200

    fixed com.straio.meta.driver.ConnectTest.java to meet sonar requirements

--- meta-driver/src/test/java/com/stratio/meta/driver/ConnectTest.java
+++ meta-driver/src/test/java/com/stratio/meta/driver/ConnectTest.java
@@ -36,38 +36,41 @@ import com.stratio.meta.common.result.Result;
 
 public class ConnectTest extends DriverParentTest {
 
-  private final static Logger logger = Logger.getLogger(ConnectTest.class);
+  private final static Logger LOGGER = Logger.getLogger(ConnectTest.class);
+  private final String keyspace = "ks_demo";
+  private final String exceptionNotExpected = "Exception not expected";
+  private final String query = "query";
 
   @BeforeClass
   public void executeDropTestBefore() {
 
     try {
-      driver.executeQuery("ks_demo", "drop table demo ;");
+      driver.executeQuery(keyspace, "drop table demo ;");
     } catch (ConnectionException | ParsingException | ValidationException | ExecutionException
         | UnsupportedException e) {
-      logger.info("Not removing table demo as it does not exists.");
+      LOGGER.info("Not removing table demo as it does not exists.");
     }
 
     try {
-      driver.executeQuery("ks_demo", "drop keyspace ks_demo ;");
+      driver.executeQuery(keyspace, "drop keyspace ks_demo ;");
     } catch (ConnectionException | ParsingException | ValidationException | ExecutionException
         | UnsupportedException e) {
-      logger.info("Not removing ks_demo as it does not exists.");
+      LOGGER.info("Not removing ks_demo as it does not exists.");
     }
 
     String msg =
         "create KEYSPACE ks_demo WITH replication = {class: SimpleStrategy, replication_factor: 1};";
     try {
-      driver.executeQuery("ks_demo", msg);
+      driver.executeQuery(keyspace, msg);
     } catch (ConnectionException | ParsingException | ValidationException | ExecutionException
         | UnsupportedException e) {
-      System.err.println("Cannot create test keyspace.");
+      LOGGER.info("Cannot create test keyspace.");
     }
 
     try {
       Thread.sleep(1000);
     } catch (InterruptedException e) {
-      logger.error("Exception", e);
+      LOGGER.error("Exception", e);
     }
 
   }
@@ -79,8 +82,8 @@ public class ConnectTest extends DriverParentTest {
     try {
       metaResult = driver.connect("TEST_USER");
     } catch (ConnectionException e) {
-      logger.error("Exception", e);
-      fail("Exception not expected");
+      LOGGER.error("Exception", e);
+      fail(exceptionNotExpected);
     }
     assertFalse(metaResult.hasError());
     ConnectResult r = ConnectResult.class.cast(metaResult);
@@ -90,32 +93,32 @@ public class ConnectTest extends DriverParentTest {
 
 
 
-  @Test(groups = {"query", "create Ks"}, dependsOnGroups = {"connect"})
-  public void ExecuteCreatewitherrorTest() {
+  @Test(groups = {query, "create Ks"}, dependsOnGroups = {"connect"})
+  public void executeCreatewitherrorTest() {
     String msg =
         "create KEYSPAC ks_demo WITH replication = "
             + "{class: SimpleStrategy, replication_factor: 1};";
     try {
-      driver.executeQuery("ks_demo", msg);
+      driver.executeQuery(keyspace, msg);
       fail("Expecting ParsingException");
     } catch (ParsingException e) {
-      logger.info(e);
+      LOGGER.info(e);
     } catch (ConnectionException | ValidationException | ExecutionException | UnsupportedException e) {
-      logger.error("Exception", e);
+      LOGGER.error("Exception", e);
       fail("Expecting ParsingException");
     }
   }
 
-  @Test(groups = {"query", "use"}, dependsOnGroups = {"create Ks"})
+  @Test(groups = {query, "use"}, dependsOnGroups = {"create Ks"})
   public void executeUseKsest() {
     String msg = "use ks_demo ;";
     Result metaResult = null;
     try {
-      metaResult = driver.executeQuery("ks_demo", msg);
+      metaResult = driver.executeQuery(keyspace, msg);
     } catch (ConnectionException | ParsingException | ValidationException | ExecutionException
         | UnsupportedException e) {
-      logger.error("Exception", e);
-      fail("Exception not expected");
+      LOGGER.error("Exception", e);
+      fail(exceptionNotExpected);
     }
     assertTrue(QueryResult.class.isInstance(metaResult), "Invalid result type");
     QueryResult r = QueryResult.class.cast(metaResult);
@@ -123,43 +126,43 @@ public class ConnectTest extends DriverParentTest {
     assertEquals(r.getCurrentCatalog(), "ks_demo", "New keyspace should be used");
   }
 
-  @Test(groups = {"query", "create Tb"}, dependsOnGroups = {"use"})
+  @Test(groups = {query, "create Tb"}, dependsOnGroups = {"use"})
   public void executeCreateTableTest() {
     String msg = "create TABLE demo (field1 varchar PRIMARY KEY , field2 varchar);";
     Result metaResult = null;
     try {
-      metaResult = driver.executeQuery("ks_demo", msg);
+      metaResult = driver.executeQuery(keyspace, msg);
     } catch (ConnectionException | ParsingException | ValidationException | ExecutionException
         | UnsupportedException e) {
-      logger.error("Exception", e);
-      fail("Exception not expected");
+      LOGGER.error("Exception", e);
+      fail(exceptionNotExpected);
     }
     assertFalse(metaResult.hasError(), "\n\nerror message is:\n" + getErrorMessage(metaResult)
         + "\n\n");
   }
 
-  @Test(groups = {"query", "insert"}, dependsOnGroups = {"create Tb"})
+  @Test(groups = {query, "insert"}, dependsOnGroups = {"create Tb"})
   public void executeInsertTest() {
     String msg = "insert into demo (field1, field2) values ('test1','text2');";
     Result metaResult = null;
     try {
-      metaResult = driver.executeQuery("ks_demo", msg);
+      metaResult = driver.executeQuery(keyspace, msg);
     } catch (ConnectionException | ParsingException | ValidationException | ExecutionException
         | UnsupportedException e) {
-      logger.error("Exception", e);
-      fail("Exception not expected");
+      LOGGER.error("Exception", e);
+      fail(exceptionNotExpected);
     }
     assertFalse(metaResult.hasError(), "\n\nerror message is:\n" + getErrorMessage(metaResult)
         + "\n\n");
   }
 
-  @Test(groups = "disconnect", dependsOnGroups = {"query"})
+  @Test(groups = "disconnect", dependsOnGroups = {query})
   public void disconnect() {
     try {
       driver.disconnect();
     } catch (ConnectionException e) {
-      logger.error("Exception", e);
-      fail("Exception not expected");
+      LOGGER.error("Exception", e);
+      fail(exceptionNotExpected);
     }
   }
 }
