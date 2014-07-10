d0e026e2170cbfe7c4e85eb59bc5997da03f910b
dhiguero <dhiguero@stratio.com>
2014-07-10 15:15:59 +0200

    Add connector interfaces

---
+++ meta-common/src/main/java/com/stratio/meta/common/connector/IConnector.java
@@ -0,0 +1,69 @@
+/*
+ * Stratio Meta
+ *
+ * Copyright (c) 2014, Stratio, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+package com.stratio.meta.common.connector;
+
+import com.stratio.meta.common.exceptions.ConnectionException;
+import com.stratio.meta.common.exceptions.UnsupportedException;
+import com.stratio.meta.common.security.ICredentials;
+
+/**
+ * Common interface for META connectors.
+ * A connector provides implementations for storage and query engines. Notice that connectors do not
+ * need to provide both functionalities at the same time.
+ */
+public interface IConnector {
+
+  /**
+   * Get the name of the datastore required by the connector. Several connectors may declare the
+   * same datastore name.
+   * @return The name.
+   */
+  public String getDatastoreName();
+
+  /**
+   * Connect to the underlying datastore. Notice that this method maybe called several times by META
+   * and it is responsability of the connector to manage the number of existing connections.
+   * @param credentials The user credentials.
+   * @param configuration The configuration.
+   * @throws ConnectionException If the connection cannot be established.
+   */
+  public void connect(ICredentials credentials, IConfiguration configuration) throws
+                                                                                ConnectionException;
+
+  /**
+   * Close the connection with the underlying datastore.
+   * @throws ConnectionException If the close operation cannot be performed.
+   */
+  public void close() throws ConnectionException;
+
+  /**
+   * Get the storage engine.
+   * @return An implementation of {@link com.stratio.meta.common.connector.IStorageEngine}.
+   * @throws UnsupportedException If the connector does not provide this functionality.
+   */
+  public IStorageEngine getStorageEngine() throws UnsupportedException;
+
+  /**
+   * Get the query engine.
+   * @return An implementation of {@link com.stratio.meta.common.connector.IQueryEngine}.
+   * @throws UnsupportedException If the connector does not provide this functionality.
+   */
+  public IQueryEngine getQueryEngine() throws UnsupportedException;
+}
---
+++ meta-common/src/main/java/com/stratio/meta/common/security/ICredentials.java
@@ -0,0 +1,27 @@
+/*
+ * Stratio Meta
+ *
+ * Copyright (c) 2014, Stratio, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+package com.stratio.meta.common.security;
+
+/**
+ * Created by dhiguero on 10/07/14.
+ */
+public interface ICredentials {
+
+}
