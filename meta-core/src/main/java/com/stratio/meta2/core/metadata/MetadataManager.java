89a26b5630cb84c991205c5ad09e1e840c5e4b75
Alvaro Agea <alvaro.agea@gmail.com>
2014-08-12 15:34:10 +0200

    Modify metadata structure

--- meta-core/src/main/java/com/stratio/meta2/core/metadata/MetadataManager.java
+++ meta-core/src/main/java/com/stratio/meta2/core/metadata/MetadataManager.java
@@ -20,9 +20,10 @@ package com.stratio.meta2.core.metadata;
 
 
 import java.util.Map;
+
 import com.stratio.meta2.metadata.CatalogMetadata;
-import com.stratio.meta2.metadata.IMetadata;
-import com.stratio.meta2.metadata.StorageMetadata;
+import com.stratio.meta2.metadata.ClusterMetadata;
+import com.stratio.meta2.metadata.TableMetadata;
 import com.stratio.meta2.transaction.ITransactionContext;
 import com.stratio.meta2.transaction.ITransactionContextFactory;
 
@@ -32,7 +33,8 @@ public enum  MetadataManager {
   MANAGER;
   private boolean isInit = false;
 
-  private Map<String, IMetadata> metadata;
+  private Map<String, CatalogMetadata> catalogs;
+  private Map<String, ClusterMetadata> clusters;
   private ITransactionContextFactory factory;
 
   private void shouldBeInit(){
@@ -41,43 +43,105 @@ public enum  MetadataManager {
     }
   }
 
-  private void shouldBeUnique(IMetadata iMetadata){
-    if (metadata.containsKey(iMetadata.getQualifiedName())) {
-      throw new MetadataManagerException("Entity["+ iMetadata.getQualifiedName()+"] already exists");
+  private void shouldBeUnique(CatalogMetadata metadata) {
+    if (catalogs.containsKey(metadata.getQualifiedName())) {
+      throw new MetadataManagerException("Catalog [" + metadata.getQualifiedName()
+          + "] already exists");
     }
   }
 
-  public synchronized void init(Map<String,IMetadata> metadata, ITransactionContextFactory factory){
-    if (metadata != null && factory != null) {
-      this.metadata = metadata;
+  private void shouldBeUnique(ClusterMetadata metadata) {
+    if (clusters.containsKey(metadata.getQualifiedName())) {
+      throw new MetadataManagerException("Storage [" + metadata.getQualifiedName()
+          + "] already exists");
+    }
+  }
+
+  private void shouldExistCatalog(String catalogQualifiedName) {
+    if (!catalogs.containsKey(catalogQualifiedName)) {
+      throw new MetadataManagerException("Catalog [" + catalogQualifiedName + "] doesn't exist yet");
+    }
+  }
+
+  private void shouldExistCluster(String catalogQualifiedName) {
+    if (!clusters.containsKey(catalogQualifiedName)) {
+      throw new MetadataManagerException("Cluster [" + catalogQualifiedName + "] doesn't exist yet");
+    }
+  }
+
+  public synchronized void init(Map<String, CatalogMetadata> catalogs,
+      Map<String, ClusterMetadata> clusters, ITransactionContextFactory factory) {
+    if (catalogs != null && clusters != null && factory != null) {
+      this.catalogs = catalogs;
+      this.clusters = clusters;
       this.factory = factory;
       this.isInit = true;
     } else {
-      throw new MetadataManagerException("[metadata] and [lock] must be NOT NULL");
+      throw new NullPointerException("Any parameter can't be NULL");
     }
   }
 
-  public void CreateCatalog(CatalogMetadata catalogMetadata) {
+  public void createCatalog(CatalogMetadata catalogMetadata) {
     shouldBeInit();
     shouldBeUnique(catalogMetadata);
     ITransactionContext context= factory.newTransactionContext();
     context.beginTransaction();
     try {
-      metadata.put(catalogMetadata.getQualifiedName(), catalogMetadata);
+      catalogs.put(catalogMetadata.getQualifiedName(), catalogMetadata);
       context.commitTransaction();
     }catch (Exception ex){
       context.rollbackTransaction();
-      throw ex;
+      throw new MetadataManagerException(ex.getMessage(), ex.getCause());
     }
   }
 
-  public void CreateStorage(StorageMetadata storageMetadata){
+  public void createCluster(ClusterMetadata clusterMetadata) {
     shouldBeInit();
-    shouldBeUnique(storageMetadata);
+    shouldBeUnique(clusterMetadata);
+    ITransactionContext context = factory.newTransactionContext();
+    context.beginTransaction();
+    try {
+      clusters.put(clusterMetadata.getQualifiedName(), clusterMetadata);
+      context.commitTransaction();
+    } catch (Exception ex) {
+      context.rollbackTransaction();
+      throw new MetadataManagerException(ex.getMessage(), ex.getCause());
+    }
+
 
   }
 
+  public void createTable(String clusterQualifiedName, String catalogQualifiedName,
+      TableMetadata tableMetadata) {
+    shouldBeInit();
+    shouldExistCatalog(catalogQualifiedName);
+    shouldExistCluster(clusterQualifiedName);
+    ITransactionContext context = factory.newTransactionContext();
+    context.beginTransaction();
+    try {
+      ClusterMetadata clusterMetadata = clusters.get(clusterQualifiedName);
+      CatalogMetadata catalogMetadata = catalogs.get(catalogQualifiedName);
 
+      if (clusterMetadata.getTables().containsKey(tableMetadata.getQualifiedName())
+          || catalogMetadata.getTables().containsKey(tableMetadata.getQualifiedName())) {
+        throw new MetadataManagerException("Table [" + tableMetadata.getQualifiedName()
+            + "] already exists");
+      }
+      clusterMetadata.getTables().put(tableMetadata.getQualifiedName(),tableMetadata);
+      catalogMetadata.getTables().put(tableMetadata.getQualifiedName(),tableMetadata);
+      clusters.remove(clusterQualifiedName);
+      clusters.put(clusterQualifiedName,clusterMetadata);
+      catalogs.remove(catalogQualifiedName);
+      catalogs.put(catalogQualifiedName,catalogMetadata);
+      context.commitTransaction();
+    } catch (Exception ex) {
+      context.rollbackTransaction();
+      throw new MetadataManagerException(ex.getMessage(), ex.getCause());
+    }
+
+
+
+  }
 
 
 
