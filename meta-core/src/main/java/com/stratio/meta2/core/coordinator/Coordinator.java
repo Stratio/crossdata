76f2e6af2f67fab27e4583b964a8bb216f4861d3
ivdiaz <idiaz@stratio.com>
2014-09-11 10:50:24 +0200

    added coordinator package

---
+++ meta-core/src/main/java/com/stratio/meta2/core/coordinator/Coordinator.java
@@ -0,0 +1,20 @@
+package com.stratio.meta2.core.coordinator;
+
+import org.apache.log4j.Logger;
+
+import com.stratio.meta2.core.query.InProgressQuery;
+import com.stratio.meta2.core.query.PlannedQuery;
+
+public class Coordinator {
+
+  /**
+   * Class logger.
+   */
+  private static final Logger LOG = Logger.getLogger(Coordinator.class);
+  
+  public InProgressQuery coordinate(PlannedQuery plannedQuery) {
+    
+    return null;
+  }
+
+}
