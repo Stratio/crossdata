139fe3073c23f2c4962c23d375fc50caf200ced2
Carlos <carlopolo@gmail.com>
2014-09-12 11:14:16 +0200

    scalamock dependency

--- meta-server/src/test/scala/com/stratio/meta2/server/coordinator/CoordinatorTest.scala
+++ meta-server/src/test/scala/com/stratio/meta2/server/coordinator/CoordinatorTest.scala
@@ -1,17 +1,15 @@
 package com.stratio.meta2.server.coordinator
 
 import org.scalatest.FunSuite
+import org.scalatest.FlatSpec
 import com.stratio.meta.common.connector.IConnector
+import org.scalamock._
 import org.scalamock.scalatest.MockFactory
-
  
-//class CoordinatorTest extends FunSuite with MockFactory {
-class CoordinatorTest extends FunSuite{
+class CoordinatorTest extends FunSuite with MockFactory{
 	test("Basic Connector Mock") {
-		//val m = mock[IConnector]
-		//(m.getConnectorName _).expects().returning("My New Connector")
-		//assert(m.getConnectorName().equals("My New Connector"))
+		val m = mock[IConnector]
+		(m.getConnectorName _).expects().returning("My New Connector")
+		assert(m.getConnectorName().equals("My New Connector"))
 	}
-
-    
 }
