348681500c24d4a09378d169ef1a75afc6c0f0b1
dhiguero <dhiguero@stratio.com>
2014-10-23 12:32:13 +0200

    Fix copyright and minor issues

--- crossdata-common/src/main/java/com/stratio/crossdata/common/exceptions/validation/NotControlledValidationException.java
+++
@@ -1,9 +0,0 @@
-package com.stratio.crossdata.common.exceptions.validation;
-
-import com.stratio.crossdata.common.exceptions.ValidationException;
-
-public class NotControlledValidationException extends ValidationException {
-    public NotControlledValidationException(Exception ex) {
-        super(ex);
-    }
-}
