620f962caffbc796c620a077b9d80425ec9e2aa1
jjlopezm <jjlopez@stratio.com>
2014-10-24 14:22:25 +0200

    ExecutionManager Tests

--- crossdata-core/src/test/java/com/stratio/crossdata/core/execution/ExecutionManagerTest.java
+++ crossdata-core/src/test/java/com/stratio/crossdata/core/execution/ExecutionManagerTest.java
@@ -1,7 +1,55 @@
+/*
+ * Licensed to STRATIO (C) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.  The STRATIO (C) licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.stratio.crossdata.core.execution;
 
-/**
- * Created by jjlopez on 24/10/14.
- */
-public class ExecutionManagerTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.stratio.crossdata.core.metadata.MetadataManagerTestHelper;
+
+public class ExecutionManagerTest extends MetadataManagerTestHelper {
+
+    @Test
+    public void testExist() throws Exception {
+        ExecutionManager.MANAGER.createEntry("key", "pruebaKey");
+        ExecutionManager.MANAGER.createEntry("key", "pruebaKey",true);
+        Assert.assertTrue(ExecutionManager.MANAGER.exists("key"));
+
+    }
+
+    @Test
+    public void testClear() throws Exception {
+        ExecutionManager.MANAGER.createEntry("key", "pruebaKey");
+        ExecutionManager.MANAGER.clear();
+        Assert.assertFalse(ExecutionManager.MANAGER.exists("key"));
+    }
+
+
+    @Test
+    public void testClearWithDelete() throws Exception {
+        ExecutionManager.MANAGER.createEntry("key", "pruebaKey");
+        ExecutionManager.MANAGER.deleteEntry("key");
+        Assert.assertFalse(ExecutionManager.MANAGER.exists("key"));
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+        ExecutionManager.MANAGER.createEntry("key2", "pruebaKey");
+        Assert.assertEquals(ExecutionManager.MANAGER.getValue("key2"), "pruebaKey");
+    }
 }
