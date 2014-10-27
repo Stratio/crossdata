6b69f9ef06759a23f2856d379acd07352b8b88cb
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2014-10-27 08:40:49 +0100

    Metadata Manager Constructor test fixed

--- crossdata-core/src/test/java/com/stratio/crossdata/core/metadata/MetadataManagerConstructorTest.java
+++ crossdata-core/src/test/java/com/stratio/crossdata/core/metadata/MetadataManagerConstructorTest.java
@@ -1,11 +1,20 @@
 package com.stratio.crossdata.core.metadata;
 
+import static org.testng.Assert.fail;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.stratio.crossdata.common.data.DataStoreName;
+import com.stratio.crossdata.common.manifest.PropertyType;
+import com.stratio.crossdata.common.metadata.DataStoreMetadata;
 
 public class MetadataManagerConstructorTest {
 
-    /*
-    @Test(expectedExceptions = MetadataManagerException.class)
+    @BeforeTest
     public void testShouldBeInitException(){
         DataStoreName name = new DataStoreName("dataStoreTest");
         String version = "0.1.0";
@@ -13,9 +22,13 @@ public class MetadataManagerConstructorTest {
         Set<PropertyType> othersProperties = new HashSet<>();
         Set<String> behaviors = new HashSet<>();
         DataStoreMetadata dataStore = new DataStoreMetadata(name, version, requiredProperties, othersProperties, behaviors);
-        MetadataManager.MANAGER.createDataStore(dataStore);
+        try {
+            MetadataManager.MANAGER.createDataStore(dataStore);
+        } catch (MetadataManagerException me) {
+            return;
+        }
+        fail();
     }
-    */
 
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void testConstructorException(){
