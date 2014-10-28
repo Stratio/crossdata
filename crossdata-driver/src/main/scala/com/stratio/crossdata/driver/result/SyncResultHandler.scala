471aff5ddb4e0bcd3b7a09fba7d367eae9f6b0fd
antonioalcocer <aalcocer@paradigmatecnologico.com>
2014-10-28 18:13:23 +0100

    CROSSDATA-172: warning scala style import with ._

--- crossdata-driver/src/main/scala/com/stratio/crossdata/driver/result/SyncResultHandler.scala
+++ crossdata-driver/src/main/scala/com/stratio/crossdata/driver/result/SyncResultHandler.scala
@@ -18,8 +18,9 @@
 
 package com.stratio.crossdata.driver.result
 
-import com.stratio.crossdata.common.exceptions.{ExecutionException, ParsingException, UnsupportedException, ValidationException}
-import com.stratio.crossdata.common.result._
+import com.stratio.crossdata.common.exceptions.{ExecutionException, ParsingException, UnsupportedException,
+ValidationException}
+import com.stratio.crossdata.common.result.{IResultHandler, QueryStatus, QueryResult, Result}
 import com.stratio.crossdata.common.result.ErrorResult
 import com.stratio.crossdata.common.data.ResultSet
 import org.apache.log4j.Logger
