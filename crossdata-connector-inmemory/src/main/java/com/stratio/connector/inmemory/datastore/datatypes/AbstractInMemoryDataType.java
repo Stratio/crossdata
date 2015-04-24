eeb205d6729c7e4719d9ff293d6a67b7ae40e2cf
luismcl <lcisneros@stratio.com>
2015-04-24 12:29:58 +0200

    InMemoryConnector throws an exception when try to create a table with not supported data type

--- crossdata-connector-inmemory/src/main/java/com/stratio/connector/inmemory/datastore/datatypes/AbstractInMemoryDataType.java
+++ crossdata-connector-inmemory/src/main/java/com/stratio/connector/inmemory/datastore/datatypes/AbstractInMemoryDataType.java
@@ -1,5 +1,6 @@
 package com.stratio.connector.inmemory.datastore.datatypes;
 
+import com.stratio.crossdata.common.metadata.DataType;
 import org.apache.log4j.Logger;
 
 import com.stratio.crossdata.common.exceptions.ExecutionException;
@@ -21,11 +22,17 @@ public abstract class AbstractInMemoryDataType {
 
     public abstract String convertInMemoryDataTypeToString(Object input);
 
-    public static AbstractInMemoryDataType castToNativeDataType(String dataType){
+    public static AbstractInMemoryDataType castToNativeDataType(String dataType)  throws ExecutionException{
         AbstractInMemoryDataType nativeDataType = null;
         if(dataType != null){
             if(dataType.equals("Date")){
                 nativeDataType = new Date();
+            }else{
+                try {
+                    DataType.valueOf(dataType);
+                }catch (IllegalArgumentException e){
+                    throw new ExecutionException("You have an error in your SQL syntax, the type '" + dataType + "' is not supported");
+                }
             }
         }
         return nativeDataType;
