f88e6656ff9011b61dbd8e6ace6b100828d17f12
luismcl <lcisneros@stratio.com>
2015-04-29 17:31:39 +0200

    Fix Crossdata-628, AbstractInMemoryDataType validation

--- crossdata-connector-inmemory/src/main/java/com/stratio/connector/inmemory/datastore/datatypes/AbstractInMemoryDataType.java
+++ crossdata-connector-inmemory/src/main/java/com/stratio/connector/inmemory/datastore/datatypes/AbstractInMemoryDataType.java
@@ -5,6 +5,9 @@ import org.apache.log4j.Logger;
 
 import com.stratio.crossdata.common.exceptions.ExecutionException;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public abstract class AbstractInMemoryDataType {
 
     /**
@@ -12,6 +15,15 @@ public abstract class AbstractInMemoryDataType {
      */
     protected static final Logger LOG = Logger.getLogger(AbstractInMemoryDataType.class);
 
+    private static final Set<String> SUPPORTED_TYPES = new HashSet();
+    static{
+        SUPPORTED_TYPES.add(Boolean.class.getSimpleName());
+        SUPPORTED_TYPES.add(String.class.getSimpleName());
+        SUPPORTED_TYPES.add(Float.class.getSimpleName());
+        SUPPORTED_TYPES.add(Integer.class.getSimpleName());
+        SUPPORTED_TYPES.add(Double.class.getSimpleName());
+        SUPPORTED_TYPES.add(Long.class.getSimpleName());
+    }
     public abstract Class<?> getClazz();
 
     public abstract void setClazz(Class<?> clazz);
@@ -27,12 +39,8 @@ public abstract class AbstractInMemoryDataType {
         if(dataType != null){
             if(dataType.equals("Date")){
                 nativeDataType = new Date();
-            }else{
-                try {
-                    DataType.valueOf(dataType);
-                }catch (IllegalArgumentException e){
-                    throw new ExecutionException("You have an error in your SQL syntax, the type '" + dataType + "' is not supported");
-                }
+            }else if (!SUPPORTED_TYPES.contains(dataType)) {
+                throw new ExecutionException("You have an error in your SQL syntax, the type '" + dataType + "' is not supported");
             }
         }
         return nativeDataType;
