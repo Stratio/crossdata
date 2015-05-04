efc77783362def4f4a4651406a53ef7de7b9f605
luismcl <lcisneros@stratio.com>
2015-05-04 19:05:08 +0200

    Add crossdata-commons coverage Issue CROSSDATA-659

---
+++ crossdata-common/src/test/java/com/stratio/crossdata/common/connector/ObservableMapTest.java
@@ -0,0 +1,7 @@
+package com.stratio.crossdata.common.connector;
+
+/**
+ * Created by lcisneros on 4/05/15.
+ */
+public class ObservableMapTest {
+}
---
+++ crossdata-common/src/test/java/com/stratio/crossdata/common/logicalplan/GroupByTest.java
@@ -0,0 +1,7 @@
+package com.stratio.crossdata.common.logicalplan;
+
+/**
+ * Created by lcisneros on 4/05/15.
+ */
+public class GroupByTest {
+}
