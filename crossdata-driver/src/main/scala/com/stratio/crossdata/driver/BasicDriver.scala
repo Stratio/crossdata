78ed02f462e50b18cbd8290cab893d386e666215
darroyocazorla <darroyo@stratio.com>
2015-07-13 14:53:50 +0200

    bug fixed: method execute wait for building up paginated results
    Conflicts:
    	crossdata-client-examples/pom.xml
    	crossdata-common/pom.xml
    	crossdata-connector-inmemory/pom.xml
    	crossdata-connector-maven-plugin/pom.xml
    	crossdata-connector/pom.xml
    	crossdata-core/pom.xml
    	crossdata-dist/pom.xml
    	crossdata-driver/pom.xml
    	crossdata-driver/src/main/scala/com/stratio/crossdata/driver/BasicDriver.scala
    	crossdata-server/pom.xml
    	crossdata-shell/pom.xml
    	pom.xml

--- crossdata-driver/src/main/scala/com/stratio/crossdata/driver/BasicDriver.scala
+++ crossdata-driver/src/main/scala/com/stratio/crossdata/driver/BasicDriver.scala
@@ -29,6 +29,7 @@ import com.stratio.crossdata.communication.Disconnect
 import com.stratio.crossdata.driver.actor.ProxyActor
 import com.stratio.crossdata.driver.config.{BasicDriverConfig, DriverConfig, DriverSectionConfig, ServerSectionConfig}
 import com.stratio.crossdata.driver.utils.RetryPolitics
+import com.stratio.crossdata.driver.result.{PaginationSyncDriverResultHandler, SyncDriverResultHandler}
 import org.apache.log4j.Logger
 
 import scala.concurrent.duration._
@@ -180,6 +181,7 @@ class BasicDriver(basicDriverConfig: BasicDriverConfig) {
       }
     }
   }
+
 
   /**
    * Shutdown actor system.
