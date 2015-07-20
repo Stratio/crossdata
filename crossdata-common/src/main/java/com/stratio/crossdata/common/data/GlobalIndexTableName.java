5959f654e3d9141f5bfff49b0aa6b10758656567
luismcl <lcisneros@stratio.com>
2015-07-20 09:23:08 +0200

    Global Index Search working

---
+++ crossdata-common/src/main/java/com/stratio/crossdata/common/data/GlobalIndexTableName.java
@@ -0,0 +1,7 @@
+package com.stratio.crossdata.common.data;
+
+/**
+ * Created by lcisneros on 14/07/15.
+ */
+public class GlobalIndexTableName {
+}
---
+++ crossdata-core/src/main/java/com/stratio/crossdata/core/planner/utils/GlobalIndexQueryBuilder.java
@@ -0,0 +1,7 @@
+package com.stratio.crossdata.core.planner.utils;
+
+/**
+ * Created by lcisneros on 14/07/15.
+ */
+public class GlobalIndexQueryBuilder {
+}
