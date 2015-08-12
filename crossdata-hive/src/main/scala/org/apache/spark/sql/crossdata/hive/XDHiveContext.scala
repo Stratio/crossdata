626fb75f107a5c822ad5d6169d66b370703345b3
darroyocazorla <darroyo@stratio.com>
2015-08-12 11:39:11 +0200

    basic test to XDHiveContext

--- crossdata-hive/src/main/scala/org/apache/spark/sql/crossdata/hive/XDHiveContext.scala
+++ crossdata-hive/src/main/scala/org/apache/spark/sql/crossdata/hive/XDHiveContext.scala
@@ -1,20 +1,25 @@
-/*
- * Copyright (C) 2015 Stratio (http://stratio.com)
+// scalastyle:off
+/* Modification and adapations - Copyright (C) 2015 Stratio (http://stratio.com)
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+// scalastyle:on
 
 package org.apache.spark.sql.crossdata.hive
+
+import java.util.concurrent.atomic.AtomicReference
 
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.DataFrame
@@ -33,4 +38,45 @@ class XDHiveContext(sc: SparkContext) extends HiveContext(sc) {
     XDDataframe(this, parseSql(sqlText))
   }
 }
+
+/**
+ * This XDHiveContext object contains utility functions to create a singleton XDContext instance,
+ * or to get the last created XDHiveContext instance.
+ */
+object XDHiveContext {
+
+  private val INSTANTIATION_LOCK = new Object()
+
+  /**
+   * Reference to the last created SQLContext.
+   */
+  @transient private val lastInstantiatedContext = new AtomicReference[XDHiveContext]()
+
+  /**
+   * Get the singleton SQLContext if it exists or create a new one using the given SparkContext.
+   * This function can be used to create a singleton SQLContext object that can be shared across
+   * the JVM.
+   */
+  def getOrCreate(sparkContext: SparkContext): XDHiveContext = {
+    INSTANTIATION_LOCK.synchronized {
+      Some(lastInstantiatedContext.get()).getOrElse(new XDHiveContext(sparkContext))
+    }
+    lastInstantiatedContext.get()
+  }
+
+  private[sql] def clearLastInstantiatedContext(): Unit = {
+    INSTANTIATION_LOCK.synchronized {
+      // scalastyle:off
+      lastInstantiatedContext.set(null)
+      // scalastyle:on
+    }
+  }
+
+  private[sql] def setLastInstantiatedContext(xdHiveContext: XDHiveContext): Unit = {
+    INSTANTIATION_LOCK.synchronized {
+      lastInstantiatedContext.set(xdHiveContext)
+    }
+  }
+}
+
 
---
+++ crossdata-hive/src/test/scala/org/apache/spark/sql/crossdata/hive/XDHiveContextIT.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.spark.sql.crossdata.hive
+
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.crossdata.XDDataframe
+import org.junit.runner.RunWith
+import org.scalatest.{BeforeAndAfterAll, FlatSpec}
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class XDHiveContextIT extends FlatSpec with BeforeAndAfterAll{
+
+  private lazy val xctx = org.apache.spark.sql.crossdata.hive.test.TestXDHiveContext
+
+  "A XDContext" should "perform a collect with a collection" in {
+    import xctx.implicits._
+
+    val df = xctx.sparkContext.parallelize((1 to 5).map(i => new String(s"val_$i"))).toDF()
+    // Any RDD containing case classes can be registered as a table.  The schema of the table is
+    // automatically inferred using scala reflection.
+    df.registerTempTable("records")
+
+    // Once tables have been registered, you can run SQL queries over them.
+    val result: Array[Row] = xctx.sql("SELECT * FROM records").collect()
+    assert(result.length === 5)
+  }
+
+
+  it must "return a XDDataFrame when executing a SQL query" in {
+    import xctx.implicits._
+
+    val df = xctx.sparkContext.parallelize((1 to 5).map(i => new String(s"val_$i"))).toDF()
+    df.registerTempTable("records")
+
+    val dataframe = xctx.sql("SELECT * FROM records")
+    assert(dataframe.isInstanceOf[XDDataframe])
+  }
+
+
+}
+
