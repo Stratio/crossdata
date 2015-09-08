3c566f4437bf7e7b27d075bc824b5de616b93f3a
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2015-09-08 15:48:49 +0200

    Some improvements in integration tests

--- crossdata-tests/src/test/scala/cassandra/CassandraTestUtils.scala
+++ crossdata-tests/src/test/scala/cassandra/CassandraTestUtils.scala
@@ -1,6 +1,6 @@
 package cassandra
 
-import com.datastax.driver.core.{Cluster, Session}
+import com.datastax.driver.core.{TableMetadata, KeyspaceMetadata, Cluster, Session}
 import cucumber.api.Scenario
 import cucumber.api.scala.{EN, ScalaDsl}
 
@@ -42,15 +42,11 @@ class CassandraTestUtils extends ScalaDsl with EN with DefaultConstants {
 
   private def buildTable(session: Session): Unit = {
 
-    session.getCluster.getMetadata.getKeyspace(Catalog).getTables
+    session.execute(s"CREATE KEYSPACE IF NOT EXISTS $Catalog WITH replication = " +
+      s"{'class':'SimpleStrategy', 'replication_factor':1}  AND durable_writes = true;")
 
-    // TODO Check if session.getCluster.getMetadata.getKeyspaces contains $Catalog
-
-    session.execute(s"CREATE KEYSPACE $Catalog WITH replication = {'class':'SimpleStrategy', 'replication_factor':1}  AND durable_writes = true;")
-
-    // TODO Check if session.getCluster.getMetadata.getKeyspace(Catalog).getTables contains $Table
-
-    session.execute(s"CREATE TABLE $Catalog.$Table (id int PRIMARY KEY, age int,comment text, enrolled boolean, name text)")
+    session.execute(s"CREATE TABLE IF NOT EXISTS $Catalog.$Table " +
+      s"(id int PRIMARY KEY, age int,comment text, enrolled boolean, name text)")
 
     for (a <- 1 to 10) {
       session.execute("INSERT INTO " + Catalog + "." + Table + " (id, age, comment, enrolled, name) VALUES " +
