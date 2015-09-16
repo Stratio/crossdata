4154d1dc53276b2d19de9a8169e277a9c24894d2
darroyocazorla <darroyo@stratio.com>
2015-09-16 20:00:56 +0200

    first mongo IT

---
+++ crossdata-mongodb/src/test/scala/com/stratio/crossdata/sql/sources/mongodb/MongoConnectorIT.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.stratio.crossdata.sql.sources.mongodb
+
+import org.apache.spark.sql.crossdata.ExecutionType._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class MongoConnectorIT extends MongoWithSharedContext {
+
+  "The Mongo connector" should "execute natively a select *" in {
+    assume(isEnvironmentReady, "MongoDB and Spark must be up and running")
+    val result = sql(s"SELECT * FROM $Collection ").collect(Native)
+    result should have length 10
+  }
+}
+
+
+
+
+
