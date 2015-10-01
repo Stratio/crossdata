ff0e8ee6aabc87923839988cea57b7f836c2f541
darroyocazorla <darroyo@stratio.com>
2015-10-01 16:16:38 +0200

    improve code style

--- crossdata-driver/src/main/scala/com/stratio/crossdata/driver/config/DriverConfig.scala
+++ crossdata-driver/src/main/scala/com/stratio/crossdata/driver/config/DriverConfig.scala
@@ -20,24 +20,28 @@ import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.log4j.Logger
 
 object DriverConfig {
-  val DRIVER_CONFIG_FILE = "driver-reference.conf"
-  val PARENT_CONFIG_NAME = "crossdata-driver"
-  val DRIVER_CONFIG_RESOURCE = "external.config.resource"
+  val DriverConfigFile = "driver-reference.conf"
+  val ParentConfigName = "crossdata-driver"
+  val DriverConfigResource = "external.config.resource"
+  val DriverConfigSeedNodes = "akka.cluster.seed-nodes"
 }
 
 trait DriverConfig {
+
+  import DriverConfig._
+
   lazy val logger: Logger = ???
 
   val config: Config = {
-    val defaultConfig = ConfigFactory.load(DriverConfig.DRIVER_CONFIG_FILE).getConfig(DriverConfig.PARENT_CONFIG_NAME)
-    val configResource = defaultConfig.getString(DriverConfig.DRIVER_CONFIG_RESOURCE)
+    val defaultConfig = ConfigFactory.load(DriverConfigFile).getConfig(ParentConfigName)
+    val configResource = defaultConfig.getString(DriverConfigResource)
 
     val mergedConfig = Some(configResource) filter { config =>
       val notFound = config.isEmpty || DriverConfig.getClass.getClassLoader.getResource(config) == null
       if (notFound) logger.warn(s"User file '$configResource' haven't been found in the classpath")
       !notFound
     } map {
-      ConfigFactory.parseResources(_).getConfig(DriverConfig.PARENT_CONFIG_NAME) withFallback defaultConfig
+      ConfigFactory.parseResources(_).getConfig(ParentConfigName) withFallback defaultConfig
     } getOrElse defaultConfig
 
 
