acd66008f8bfb5a3761018e2cf37f34638b8c466
darroyocazorla <darroyo@stratio.com>
2015-11-25 15:55:33 +0100

    test function registry

--- cassandra/src/test/scala/com/stratio/crossdata/connector/cassandra/CassandraFunctionIT.scala
+++ cassandra/src/test/scala/com/stratio/crossdata/connector/cassandra/CassandraFunctionIT.scala
@@ -26,12 +26,20 @@ class CassandraFunctionIT extends CassandraWithSharedContext {
 
   execTypes.foreach { exec =>
 
-    "The Cassandra connector" should s"be able to ${exec.toString}ly select the built-in funcions `now`, `dateOf` and `unixTimeStampOf`" in {
+    "The Cassandra connector" should s"be able to ${exec.toString}ly select the built-in functions `now`, `dateOf` and `unixTimeStampOf`" in {
       assumeEnvironmentIsUpAndRunning
 
       val query = s"SELECT cassandra_now() as t, cassandra_now() as a, cassandra_dateOf(cassandra_now()) as dt, cassandra_unixTimestampOf(cassandra_now()) as ut FROM $Table"
       sql(query).collect(exec) should have length 10
     }
   }
+
+  it should s"be able to resolve non-duplicates functions automatically without specifying the datasource" in {
+    assumeEnvironmentIsUpAndRunning
+
+    val query = s"SELECT unixTimestampOf(now()) as ut FROM $Table"
+    sql(query).collect(Native) should have length 10
+  }
+
 
 }
