cd333dcd382478a81af7e3ec5efa37d11bda2383
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2015-12-03 14:06:49 +0100

    More options added to the Crossdata deploy script

--- scripts/crossdata-deploy.sh
+++ scripts/crossdata-deploy.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 if [ "$#" -lt 1 ]; then
-    echo "Usage: crossdata-deploy.sh /path/to/conf/server-application.conf"
+    echo "Usage: crossdata-deploy.sh /path/to/confFolder/<configFile>.conf"
     exit 1
 fi
 
 configFile="$1"
 
 echo "Configuration File: $1"
+
+COMMAND="bin/spark-submit"
 
 function process_line {
     #echo "Args: $#"
@@ -23,27 +25,60 @@ function process_line {
         echo "VALUE: $VALUE"
         if [[ $KEY == spark.path ]]
         then
-            XD_SPARK_PATH=$VALUE
+            COMMAND="$VALUE/$COMMAND"
         fi
         if [[ $KEY == spark.mainClass ]]
         then
-            XD_APP_MAINCLASS=$VALUE
+            COMMAND="$COMMAND --class $VALUE"
         fi
         if [[ $KEY == spark.master ]]
         then
-            XD_SPARK_MASTER=$VALUE
+            COMMAND="$COMMAND --master $VALUE"
+        fi
+        if [[ $KEY == server.name ]]
+        then
+            COMMAND="$COMMAND --name $VALUE"
+        fi
+        if [[ $KEY == spark.proxyUser ]]
+        then
+            COMMAND="$COMMAND --proxy-user $VALUE"
+        fi
+        if [[ $KEY == spark.packages ]]
+        then
+            COMMAND="$COMMAND --packages $VALUE"
+        fi
+        if [[ $KEY == spark.driverMemory ]]
+        then
+            COMMAND="$COMMAND --driver-memory $VALUE"
+        fi
+        if [[ $KEY == spark.executorMemory ]]
+        then
+            COMMAND="$COMMAND --executor-memory $VALUE"
+        fi
+        if [[ $KEY == spark.totalCores ]]
+        then
+            COMMAND="$COMMAND --total-executor-cores $VALUE"
+        fi
+        if [[ $KEY == spark.executorCores ]]
+        then
+            COMMAND="$COMMAND --executor-cores $VALUE"
+        fi
+        if [[ $KEY == yarn.numExecutors ]]
+        then
+            COMMAND="$COMMAND --num-executors $VALUE"
         fi
         if [[ $KEY == server.config ]]
         then
-            XD_SERVER_CONFIG=$VALUE
+            COMMAND="$COMMAND --files $VALUE"
         fi
         if [[ $KEY == spark.appJar ]]
         then
-            XD_JAR_PATH=$VALUE
+            COMMAND="$COMMAND --jars $VALUE"
+            COMMAND="$COMMAND $VALUE"
         fi
         if [[ $KEY == spark.args ]]
         then
-            XD_APP_ARGS=$VALUE
+            COMMAND="$COMMAND $VALUE"
         fi
     fi
 }
@@ -58,13 +93,9 @@ do
         #echo "tmp1 = $tmp1"
         process_line $tmp1
     fi
-
 done < $1
 
-COMMAND="$XD_SPARK_PATH/bin/spark-submit --class $XD_APP_MAINCLASS --master $XD_SPARK_MASTER --files $XD_SERVER_CONFIG --jars $XD_JAR_PATH $XD_JAR_PATH $XD_APP_ARGS"
-
-echo "COMMAND=$COMMAND"
+echo "Executing: $COMMAND"
 
 $COMMAND
-
 
