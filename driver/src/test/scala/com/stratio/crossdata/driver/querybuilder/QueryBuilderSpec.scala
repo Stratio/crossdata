e68cbdf01679e584181b620046fb2db994a76d96
darroyocazorla <darroyo@stratio.com>
2015-12-10 17:37:36 +0100

    test main query builder features

--- driver/src/test/scala/com/stratio/crossdata/driver/querybuilder/QueryBuilderSpec.scala
+++ driver/src/test/scala/com/stratio/crossdata/driver/querybuilder/QueryBuilderSpec.scala
@@ -15,6 +15,8 @@
  */
 package com.stratio.crossdata.driver.querybuilder
 
+import java.sql.Date
+
 import com.stratio.crossdata.test.BaseXDTest
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -24,7 +26,7 @@ class QueryBuilderSpec extends BaseXDTest {
 
   "The Query Builder" should "be able to build a completed query using strings" in {
 
-    val query = select ("col, '1', max(col)") from "table inner join table2 on a = b" where "a = b" groupBy "col" having "a = b" orderBy "col ASC" limit 5
+    val query = select("col, '1', max(col)") from "table inner join table2 on a = b" where "a = b" groupBy "col" having "a = b" orderBy "col ASC" limit 5
 
     val expected = """
                      | SELECT col, '1', max(col)
@@ -38,6 +40,34 @@ class QueryBuilderSpec extends BaseXDTest {
 
     compareAfterFormatting(query.build, expected)
   }
+
+
+  it should "be able to add a where clause on a limited query" in {
+
+    val query = selectAll from 'table limit 1 where 'a < 5
+
+    val expected = """
+                     | SELECT * FROM table
+                     | WHERE a < 5
+                     | LIMIT 1
+                   """
+
+    compareAfterFormatting(query.build, expected)
+  }
+
+  it should "be able to add a where clause on a limited query which contains filters" in {
+
+    val query = selectAll from 'table where 'a > 10 limit 1 where 'a < 5
+
+    val expected = """
+                     | SELECT * FROM table
+                     | WHERE (a > 10) AND (a < 5)
+                     | LIMIT 1
+                   """
+
+    compareAfterFormatting(query.build, expected)
+  }
+
 
   it should "be able to join several queries" in {
 
@@ -56,12 +86,85 @@ class QueryBuilderSpec extends BaseXDTest {
 
   it should "not allow to add a filter on a combined query" in {
 
-    the [Error] thrownBy {
+    the[Error] thrownBy {
       (selectAll from 'table) unionAll (selectAll from 'table2) where "a = b"
     } should have message "Predicates cannot by applied to combined queries"
 
   }
 
+  it should "be able to build a query containing predicates with objects" in {
+
+    val query = selectAll from 'table where ('a < new Date(0) or 'a > 5)
+
+    val expected = """
+                     | SELECT * FROM table
+                     | WHERE
+                     | (a < '1970-01-01')
+                     | OR
+                     | (a > 5)
+                   """
+
+    compareAfterFormatting(query.build, expected)
+  }
+
+  it should "be able to build a query containing a subquery as a predicate" in {
+
+    val query = select('c + 4).from('table).where('col === (select('c) from 't))
+
+    val expected = """
+                     | SELECT c + 4 FROM table
+                     | WHERE col = ( SELECT c FROM t )
+                   """
+    compareAfterFormatting(query.build, expected)
+
+  }
+
+  it should " be able to build a completed query without distinct" in {
+
+    val query = select(distinct('col)) from 'test where ('quantity > 10) groupBy 'age having ('age > 25) orderBy 'age limit 10
+
+    val expected = """
+                     | SELECT DISTINCT col
+                     | FROM test
+                     | WHERE quantity > 10
+                     | GROUP BY age
+                     | HAVING age > 25
+                     | ORDER BY age
+                     | LIMIT 10
+                   """
+
+    compareAfterFormatting(query.build, expected)
+
+  }
+
+  it should " be able to build a query with a subquery" in {
+
+    val query = select("alias.name") from ((selectAll from 'table) as 'alias)
+
+    val expected = """
+                     | SELECT alias.name
+                     | FROM (
+                     | SELECT * FROM table
+                     | ) AS alias
+                   """
+
+    compareAfterFormatting(query.build, expected)
+
+  }
+
+  it should " be able to build a query with a join clause" in {
+
+    val query = select('name, 'age, 'quantity) from ('test innerJoin 'animals on "test.id = animals.id")
+
+    val expected = """
+                     | SELECT name, age, quantity
+                     | FROM test
+                     | JOIN animals
+                     | ON test.id = animals.id
+                   """
+
+    compareAfterFormatting(query.build, expected)
+  }
 
   // TODO types tests
 
@@ -71,451 +174,13 @@ class QueryBuilderSpec extends BaseXDTest {
 
   // TODO tests below
 
-  it should " be able to build a completed query with distinct" in {
 
-    //val q2 = selectAll from 'table where ('a < new Date(10) || 'a > 5) unionAll (selectAll from "table") unionDistinct   (selectAll from ('t1 as 'alias))
-
-
-    /*val query =
-      select('col + 4, 'col)
-        .from('table join 'table2 on "aaa" join 'table3 join (q1))
-        .where((select('c) from 't) && q1 && 5 === 'aa)
-        .groupBy("aa").having("a<5" && 'c)
-        .orderBy('c).limit(1) unionAll (select('c) from 'table)
-
-    //val q2 = select('c) from 't where ("a" && ("b"))
-    */
-
-
-/*    val query = QueryBuilder
-      .select()
-      .distinct(List("*"))
-      .from(List("test"))
-      .where("quantity > 10")
-      .groupBy(List("age"))
-      .having("age > 25")
-      .orderBy("age")
-      .limit("10")
-      .build()
-      .toString*/
-
-/*    val expected = formatOutput("""SELECT DISTINCT *
-                     | FROM test
-                     | WHERE quantity > 10
-                     | GROUP BY age
-                     | HAVING age > 25
-                     | ORDER BY age
-                     | LIMIT 10""")
-
-    query should be (expected)*/
+  def compareAfterFormatting(query: String, expected: String) = {
+    formatOutput(query) should be(formatOutput(expected))
   }
 
-/*  it should " be able to build a completed query without distinct" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .where("quantity > 10")
-      .groupBy(List("age"))
-      .having("age > 25")
-      .orderBy("age")
-      .limit("10")
-      .build()
-      .toString
-
-    val expected = formatOutput("""SELECT name, age, quantity
-                    | FROM test
-                    | WHERE quantity > 10
-                    | GROUP BY age
-                    | HAVING age > 25
-                    | ORDER BY age
-                    | LIMIT 10""")
-
-    query should be (expected)
-  }
-
-  it should " be able to build a query with an empty having" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .where("quantity > 10")
-      .groupBy(List("age"))
-      .having()
-      .orderBy("age")
-      .limit("10")
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                     | FROM test
-                     | WHERE quantity > 10
-                     | GROUP BY age
-                     | ORDER BY age
-                     | LIMIT 10""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with an empty group by" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .where("quantity > 10")
-      .groupBy()
-      .having()
-      .orderBy("age")
-      .limit("10")
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                     | FROM test
-                     | WHERE quantity > 10
-                     | ORDER BY age
-                     | LIMIT 10""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with an empty limit" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .where("quantity > 10")
-      .groupBy()
-      .having()
-      .orderBy("age")
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                     | FROM test
-                     | WHERE quantity > 10
-                     | ORDER BY age""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with an empty where clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .where()
-      .groupBy()
-      .having()
-      .orderBy("age")
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test
-                                  | ORDER BY age""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with an empty order by" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .where()
-      .groupBy()
-      .having()
-      .orderBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with a subquery" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List(QueryBuilder.select(true)
-            .projections(List("*"))
-            .from(List("animals"))
-            .where()
-            .groupBy()
-            .having()
-            .orderBy()
-            .limit()
-            .build()
-            .toString))
-      .where()
-      .groupBy()
-      .having()
-      .orderBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM (SELECT * FROM animals)""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with a join clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test INNER JOIN animals ON test.id = animals.id"))
-      .where()
-      .groupBy()
-      .having()
-      .orderBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test INNER JOIN animals ON test.id = animals.id""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query skipping the limit clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .distinct(List("*"))
-      .from(List("test"))
-      .where()
-      .groupBy()
-      .having()
-      .orderBy()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT DISTINCT *
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query skipping the order by clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("id, name, age"))
-      .from(List("test"))
-      .where()
-      .groupBy()
-      .having()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT id, name, age
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query skipping the having clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("id, name, age"))
-      .from(List("test"))
-      .where()
-      .groupBy()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT id, name, age
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query skipping the group by clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("id, name, age"))
-      .from(List("test"))
-      .where()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT id, name, age
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query skipping the where clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("id, name, age"))
-      .from(List("test"))
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT id, name, age
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the where clause missing from a from clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .groupBy()
-      .having()
-      .orderBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the group by clause missing from a from clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .having()
-      .orderBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the having clause missing from a from clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .orderBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the order by clause missing from a from clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the group by clause missing from a where clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .where()
-      .having()
-      .orderBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the having clause missing from a where clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .where()
-      .orderBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the order by clause missing from a where clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .where()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the having clause missing from a group by clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .groupBy()
-      .orderBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the order by clause missing from a group by clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .groupBy()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }
-
-  it should " be able to build a query with the order by clause missing from a having clause" in {
-
-    val query = QueryBuilder
-      .select()
-      .projections(List("name, age, quantity"))
-      .from(List("test"))
-      .groupBy()
-      .having()
-      .limit()
-      .build()
-      .toString
-    val expected = formatOutput("""SELECT name, age, quantity
-                                  | FROM test""")
-    query should be (expected)
-  }*/
-
-  def compareAfterFormatting(query: String, expected: String) =
-    formatOutput(query) should be (formatOutput(expected))
 
   def formatOutput(query: String): String =
-    query.stripMargin.replace(System.lineSeparator(), " ").trim.replaceAll(" +", " ")
+    query.stripMargin.replaceAll(System.lineSeparator(), " ").trim.replaceAll(" +", " ")
 
 }
