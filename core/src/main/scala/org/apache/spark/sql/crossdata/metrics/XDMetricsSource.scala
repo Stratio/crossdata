fe6eca9be7fe13571885fde1b00172bc98e0abca
pmadrigal <pedromadri89@hotmail.com>
2015-12-10 18:07:27 +0100

    license added

--- core/src/main/scala/org/apache/spark/sql/crossdata/metrics/XDMetricsSource.scala
+++ core/src/main/scala/org/apache/spark/sql/crossdata/metrics/XDMetricsSource.scala
@@ -10,9 +10,10 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions anÇd
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.spark.sql.crossdata.metrics
 
 import com.codahale.metrics.{Gauge, MetricRegistry}
