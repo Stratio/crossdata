90efde74152edc0ad3548843ecd8d3f83ff7008e
gserranojc <gserranojc@gmail.com>
2016-01-12 12:00:37 +0100

    catalog created and all methods are overrided

--- core/src/main/scala/org/apache/spark/sql/crossdata/catalog/ZookeeperCatalog.scala
+++ core/src/main/scala/org/apache/spark/sql/crossdata/catalog/ZookeeperCatalog.scala
@@ -18,13 +18,20 @@
 
 package org.apache.spark.sql.crossdata.catalog
 
+import java.util.UUID
+
+import com.stratio.common.utils.config.TypesafeConfigComponent
+import com.stratio.common.utils.logger.SparkLoggerComponent
+import com.stratio.common.utils.repository.zookeeper.ZookeeperRepositoryComponent
 import org.apache.spark.Logging
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.catalyst.{TableIdentifier, SimpleCatalystConf, CatalystConf}
+import org.apache.spark.sql.catalyst.{CatalystConf, SimpleCatalystConf, TableIdentifier}
 import org.apache.spark.sql.crossdata.XDCatalog._
+import org.apache.spark.sql.crossdata.models.TableModel
 import org.apache.spark.sql.crossdata.serializers.CrossdataSerializer
 import org.apache.spark.sql.crossdata.{XDCatalog, XDContext}
 import org.apache.spark.sql.types.StructType
+import org.json4s.jackson.Serialization._
 
 object ZookeeperCatalog {
 
@@ -33,8 +40,17 @@ object ZookeeperCatalog {
   val TablesPath = s"$BaseZKPath/tables"
   val EphemeralTablesPath = s"$BaseZKPath/ephemeraltables"
   val StreamingQueriesPath = s"$BaseZKPath/streamingqueries"
-}
 
+  def getTableFromPath(zkRepository: ZookeeperRepositoryComponent, path: String): Option[TableModel] =
+    zkRepository.repository.get(path).map(rawTable => read[TableModel](new Predef.String(rawTable)))
+
+  def insertTableIntoPath(zkRepository: ZookeeperRepositoryComponent,
+                          tableModel: TableModel,
+                          path: String): TableModel =
+    read[TableModel](new Predef.String(zkRepository.repository.create(path, write(tableModel).getBytes)))
+
+  def createId : String = UUID.randomUUID.toString
+}
 
 /**
  * Default implementation of the [[org.apache.spark.sql.crossdata.XDCatalog]] with persistence using Zookeeper.
@@ -43,48 +59,75 @@ object ZookeeperCatalog {
  */
 class ZookeeperCatalog(override val conf: CatalystConf = new SimpleCatalystConf(true), xdContext: XDContext)
   extends XDCatalog(conf, xdContext) with Logging with CrossdataSerializer
- //with ZookeeperRepositoryComponent with SparkLoggerComponent with TypesafeConfigComponent
-{
+  with ZookeeperRepositoryComponent with SparkLoggerComponent with TypesafeConfigComponent {
 
   import ZookeeperCatalog._
-
   import org.apache.spark.sql.crossdata._
 
-  private val catalogConfig = xdContext.catalogConfig
-
-  //Crear la config correcta para zookeeper
-
-  //puedo usar logger, repository y config!!
+  override val config = new TypesafeConfig(Option(xdContext.catalogConfig))
 
   override def lookupTable(tableName: String, databaseName: Option[String]): Option[CrossdataTable] = {
+    if (repository.exists(TablesPath)) {
+      val findTable = repository.getChildren(TablesPath).flatMap(id => getTableFromPath(this, s"$TablesPath/$id"))
+        .find(tableModel =>
+          databaseName.forall(dbName => tableModel.database.exists(modelDbName => modelDbName == dbName)))
 
-    Some(
-      CrossdataTable(table, Some(database), getUserSpecifiedSchema(schemaJSON), datasource, getPartitionColumn(partitionColumn), getOptions(optsJSON), version)
-    )
+      findTable match {
+        case Some(zkTable) =>
+          Option(CrossdataTable(zkTable.name,
+            zkTable.database,
+            getUserSpecifiedSchema(zkTable.schema),
+            zkTable.dataSource,
+            zkTable.partitionColumns.toArray,
+            zkTable.options,
+            zkTable.version))
+        case None =>
+          logger.warn("Table not exists")
+          None
+      }
+    } else {
+      logger.warn("Tables path not exists")
+      None
+    }
   }
 
   override def listPersistedTables(databaseName: Option[String]): Seq[(String, Boolean)] = {
+    if (repository.exists(TablesPath)) {
+      repository.getChildren(TablesPath).flatMap(id =>
+        getTableFromPath(this, s"$TablesPath/$id")
+          .map(tableModel => (tableModel.getExtendedName, false)))
+    } else {
+      logger.warn("Tables path not exists")
+      Seq.empty[(String, Boolean)]
+    }
   }
 
   override def persistTableMetadata(crossdataTable: CrossdataTable, logicalRelation: Option[LogicalPlan]): Unit = {
+    val tableId = createId
+    val tableIdentifier = TableIdentifier(crossdataTable.tableName, crossdataTable.dbName).toSeq
 
-    val tableSchema = serializeSchema(crossdataTable.userSpecifiedSchema.getOrElse(new StructType()))
-    val tableOptions = serializeOptions(crossdataTable.opts)
-    val partitionColumn = serializePartitionColumn(crossdataTable.partitionColumn)
+    insertTableIntoPath(this,
+      TableModel(
+        tableId,
+        crossdataTable.tableName,
+        serializeSchema(crossdataTable.userSpecifiedSchema.getOrElse(new StructType())),
+        crossdataTable.datasource,
+        crossdataTable.dbName,
+        crossdataTable.partitionColumn,
+        crossdataTable.opts),
+      s"$TablesPath/$tableId")
 
-
-
-    val tableIdentifier = TableIdentifier(crossdataTable.tableName,crossdataTable.dbName).toSeq
-    //Try to register the table.
-    registerTable(tableIdentifier, logicalRelation.getOrElse(lookupRelation(tableIdentifier))
+    registerTable(tableIdentifier, logicalRelation.getOrElse(lookupRelation(tableIdentifier)))
   }
 
   override def dropPersistedTable(tableName: String, databaseName: Option[String]): Unit = {
-
+    val foundIdTables = repository.getChildren(TablesPath).filter(id =>
+      getTableFromPath(this, s"$TablesPath/$id")
+        .exists(tableModel => tableName != tableModel.name ||
+          databaseName.forall(dbName => tableModel.database.exists(modelDbName => modelDbName == dbName))))
+    foundIdTables.foreach(id => repository.delete(s"$TablesPath/$id"))
   }
 
-  override def dropAllPersistedTables(): Unit = {
-
-  }
-
+  override def dropAllPersistedTables(): Unit =
+    repository.getChildren(TablesPath).foreach(id => repository.delete(s"$TablesPath/$id"))
 }
