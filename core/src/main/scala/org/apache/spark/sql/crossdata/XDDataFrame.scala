d6e200cb0df21e1cdb736daa27187b727aed3dc6
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2016-01-13 17:58:15 +0100

    Added aliases exceptions

--- core/src/main/scala/org/apache/spark/sql/crossdata/XDDataFrame.scala
+++ core/src/main/scala/org/apache/spark/sql/crossdata/XDDataFrame.scala
@@ -109,9 +109,14 @@ class XDDataFrame private[sql](@transient override val sqlContext: SQLContext,
   def flattenedCollect(): Array[Row] = {
 
     def flattenProjectedColumns(exp: Expression, prev: List[String] = Nil): (List[String], Boolean) = exp match {
-      case Alias(child, name) => List(name) -> true
-      case GetStructField(child, field, _) => flattenProjectedColumns(child, field.name :: prev)
-      case AttributeReference(name, _, _, _) => (name :: prev, false)
+      case GetStructField(child, field, _) =>
+        flattenProjectedColumns(child, field.name :: prev)
+      case AttributeReference(name, _, _, _) =>
+        (name :: prev, false)
+      case Alias(child @ GetStructField(_, StructField(fname, _, _, _), _), name) if(fname == name) =>
+        flattenProjectedColumns(child)
+      case Alias(child, name) =>
+        List(name) -> true
       case _ => prev -> false
     }
 
