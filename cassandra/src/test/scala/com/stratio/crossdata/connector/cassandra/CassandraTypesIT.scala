00fd12290a4f3279e98b0de80d0066c3548a48d1
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2016-01-22 08:51:46 +0100

    Added bidimensional flattening test for C*

--- cassandra/src/test/scala/com/stratio/crossdata/connector/cassandra/CassandraTypesIT.scala
+++ cassandra/src/test/scala/com/stratio/crossdata/connector/cassandra/CassandraTypesIT.scala
@@ -38,6 +38,7 @@ class CassandraTypesIT extends CassandraWithSharedContext with SharedXDContextTy
         s"CREATE TYPE $Catalog.STRUCT_DATE (field1 TIMESTAMP, field2 INT)"::
         s"CREATE TYPE $Catalog.STRUCT_STRUCT (field1 TIMESTAMP, field2 INT, struct1 frozen<STRUCT1>)"::
         s"CREATE TYPE $Catalog.STRUCT_DATE1 (structField1 TIMESTAMP, structField2 INT)"::
+        s"CREATE TYPE $Catalog.STRUCT_ARRAY_STRUCT (stringfield VARCHAR, arrayfield LIST<frozen<STRUCT>>)"::
         s"""
            |CREATE TABLE $Catalog.$TypesTable
            |(
@@ -67,7 +68,8 @@ class CassandraTypesIT extends CassandraWithSharedContext with SharedXDContextTy
            |  arraystruct LIST<frozen<STRUCT>>,
            |  arraystructwithdate LIST<frozen<STRUCT_DATE>>,
            |  structofstruct frozen<STRUCT_STRUCT>,
-           |  mapstruct MAP<VARCHAR, frozen<STRUCT_DATE1>>
+           |  mapstruct MAP<VARCHAR, frozen<STRUCT_DATE1>>,
+           |  arraystructarraystruct LIST<frozen<STRUCT_ARRAY_STRUCT>>
            |)
       """.stripMargin::Nil
 
@@ -75,18 +77,20 @@ class CassandraTypesIT extends CassandraWithSharedContext with SharedXDContextTy
 
     val dataQuery =
       s"""|
-        |INSERT INTO $Catalog.$TypesTable (
+          |INSERT INTO $Catalog.$TypesTable (
           |  id, int, bigint, long, string, boolean, double, float, decimalInt, decimalLong,
           |  decimalDouble, decimalFloat, date, timestamp, tinyint, smallint, binary,
           |  arrayint, arraystring, mapintint, mapstringint, mapstringstring, struct, arraystruct,
-          |  arraystructwithdate, structofstruct, mapstruct
-          |  )
+          |  arraystructwithdate, structofstruct, mapstruct, arraystructarraystruct
+          |)
           |VALUES (
           |	1,  2147483647, 9223372036854775807, 9223372036854775807, 'string', true,
           |	3.3, 3.3, 42, 42, 42.0, 42.0, '2015-11-30 10:00', '2015-11-30 10:00', 1, 1, textAsBlob('binary data'),
           |	[4, 42], ['hello', 'world'], { 0 : 1 }, {'b' : 2 }, {'a':'A', 'b':'B'}, {field1: 1, field2: 2}, [{field1: 1, field2: 2}],
           |	[{field1: 9223372036854775807, field2: 2}], {field1: 9223372036854775807, field2: 2, struct1: {structField1: 'string', structField2: 42}},
-          |	{'structid' : {structField1: 9223372036854775807, structField2: 42}}
+          |	{'structid' : {structField1: 9223372036854775807, structField2: 42}},
+          | [{stringfield: 'aa', arrayfield: [{field1: 1, field2: 2}, {field1: -1, field2: -2}]},
+          | {stringfield: 'bb', arrayfield: [{field1: 11, field2: 22}]}]
           |);
       """.stripMargin
 
