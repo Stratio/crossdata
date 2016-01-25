cff720d6ffc743df71263378c998f8b438fbc47f
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2016-01-25 17:25:11 +0100

    Timeout for tests added

--- core/src/test/scala/com/stratio/crossdata/test/BaseXDTest.scala
+++ core/src/test/scala/com/stratio/crossdata/test/BaseXDTest.scala
@@ -15,10 +15,15 @@
  */
 package com.stratio.crossdata.test
 
+import org.scalatest.concurrent.TimeLimitedTests
+import org.scalatest.time.SpanSugar._
 import org.scalatest.{FlatSpec, Matchers}
 
 /**
  * Base class for both unit and integration tests
  */
-abstract class BaseXDTest extends FlatSpec with Matchers {
+abstract class BaseXDTest extends FlatSpec with Matchers with TimeLimitedTests {
+
+  val timeLimit = 5 minutes
+
 }
