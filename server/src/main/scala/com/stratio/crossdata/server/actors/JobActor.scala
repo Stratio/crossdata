53416e1129a1a7a0e45682a108b62e23c9a01a32
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2016-02-10 14:25:45 +0100

    Server changed to the new Actor per Job approach

--- server/src/main/scala/com/stratio/crossdata/server/actors/JobActor.scala
+++ server/src/main/scala/com/stratio/crossdata/server/actors/JobActor.scala
@@ -1,10 +1,25 @@
+/**
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.stratio.crossdata.server.actors
 
-import akka.actor.{ActorRef, Actor}
+import akka.actor.{Props, ActorRef, Actor}
 import com.stratio.crossdata.common.SQLCommand
 import com.stratio.crossdata.common.result.{ErrorResult, SuccessfulQueryResult}
 import com.stratio.crossdata.server.actors.JobActor.Commands.GetJobStatus
-import com.stratio.crossdata.server.actors.JobActor.Events.{JobCompleted}
+import com.stratio.crossdata.server.actors.JobActor.Events.{JobFailed, JobCompleted}
 import org.apache.spark.sql.crossdata.{XDContext, XDDataFrame}
 
 import scala.concurrent.Future
@@ -17,22 +32,25 @@ object JobActor {
     val Starting, Running, Failed, Completed = Value
   }
 
-  private trait JobEvent
+  trait JobEvent
 
 
   private object InternalEvents {
     case class JobStarted() extends JobEvent
-    case class JobFailed(err: Throwable) extends JobEvent
   }
 
   object Events {
     case class JobCompleted() extends JobEvent
+    case class JobFailed(err: Throwable) extends JobEvent
   }
 
   object Commands {
     trait JobCommand
     case class GetJobStatus()
   }
+
+  def props(xDContext: XDContext, command: SQLCommand, requester: ActorRef): Props =
+    Props(new JobActor(xDContext, command, requester))
 
 }
 
@@ -48,6 +66,8 @@ class JobActor(
   override def preStart(): Unit = {
     super.preStart()
 
+    import context.dispatcher
+
     Future {
       val df = xdContext.sql(command.query)
       self ! JobStarted()
@@ -62,7 +82,7 @@ class JobActor(
       self ! JobCompleted()
 
     } onComplete {
-      case Failure(e @ Throwable) =>
+      case Failure(e) =>
         self ! JobFailed(e)
       case Success(_) =>
         self ! JobCompleted
@@ -76,8 +96,9 @@ class JobActor(
       sender ! status
     case JobStarted() if status == Starting =>
       context.become(receive(Running))
-    case JobFailed(e) if sender == self && (Seq(Starting, Running) contains status) =>
+    case event @ JobFailed(e) if sender == self && (Seq(Starting, Running) contains status) =>
       context.become(receive(Failed))
+      context.parent ! event
       requester ! ErrorResult(command.queryId, e.getMessage, Some(new Exception(e.getMessage)))
       throw e //Let It Crash: It'll be managed by its supervisor
     case event: JobCompleted if sender == self && status == Running =>
--- server/src/main/scala/com/stratio/crossdata/server/actors/ServerActor.scala
+++ server/src/main/scala/com/stratio/crossdata/server/actors/ServerActor.scala
@@ -15,17 +15,27 @@
  */
 package com.stratio.crossdata.server.actors
 
-import akka.actor.{Actor, Props}
+import akka.actor.SupervisorStrategy.{Stop, Restart}
+import akka.actor._
 import akka.cluster.Cluster
 import com.stratio.crossdata.common.SQLCommand
 import com.stratio.crossdata.common.result.{ErrorResult, SuccessfulQueryResult}
+import com.stratio.crossdata.server.actors.JobActor.Events.{JobCompleted, JobFailed}
 import com.stratio.crossdata.server.config.ServerConfig
 import org.apache.log4j.Logger
 import org.apache.spark.sql.crossdata.{XDDataFrame, XDContext}
 
-
 object ServerActor {
+
+
+
   def props(cluster: Cluster, xdContext: XDContext): Props = Props(new ServerActor(cluster, xdContext))
+
+  object Messages {
+
+
+  }
+
 }
 
 class ServerActor(cluster: Cluster, xdContext: XDContext) extends Actor with ServerConfig {
@@ -33,24 +43,21 @@ class ServerActor(cluster: Cluster, xdContext: XDContext) extends Actor with Ser
   override lazy val logger = Logger.getLogger(classOf[ServerActor])
 
   def receive: Receive = {
-
     case sqlCommand @ SQLCommand(query, _, withColnames) =>
       logger.debug(s"Query received ${sqlCommand.queryId}: ${sqlCommand.query}. Actor ${self.path.toStringWithoutAddress}")
-      try {
-        val df = xdContext.sql(query)
-        val rows = if(withColnames) df.asInstanceOf[XDDataFrame].flattenedCollect() //TODO: Replace this cast by an implicit conversion
-        else df.collect()
-        sender ! SuccessfulQueryResult(sqlCommand.queryId, rows, df.schema)
-      } catch {
-        case e: Throwable => {
-          logger.error(e.getMessage)
-          sender ! ErrorResult(sqlCommand.queryId, e.getMessage, Some(new Exception(e.getMessage)))
-        }
-      }
-
+      context.actorOf(JobActor.props(xdContext, sqlCommand, sender()))
+    case JobFailed(e) =>
+      logger.error(e.getMessage)
+      context.stop(sender())
+    case JobCompleted =>
+      context.stop(sender()) //TODO: This could be changed so done works could be inquired about their state
     case any =>
-      logger.error(s"Something is going wrong!. Unknown message: $any")
-
+      logger.error(s"Something is going wrong! Unknown message: $any")
   }
 
+  //TODO: Use number of tries and timeout configuration parameters
+  override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+    case _: ActorKilledException => Stop  //In the future it might be interesting to add the
+    case _ => Restart //Crashed job gets restarted
+  }
 }
