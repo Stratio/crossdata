0a85d7367b22b793559b1a0ffc5cf245d7acde4a
darroyocazorla <darroyo@stratio.com>
2016-02-19 09:22:51 +0100

    add unit test to streaming parser

--- core/src/test/scala/org/apache/spark/sql/crossdata/execution/datasources/StreamingDdlParserSpec.scala
+++ core/src/test/scala/org/apache/spark/sql/crossdata/execution/datasources/StreamingDdlParserSpec.scala
@@ -16,10 +16,10 @@
 package org.apache.spark.sql.crossdata.execution.datasources
 
 import com.stratio.crossdata.test.BaseXDTest
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.crossdata.XDContext
-import org.apache.spark.sql.crossdata.config.CoreConfig
-import org.apache.spark.sql.crossdata.test.SharedXDContextTest
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.mock.MockitoSugar
@@ -28,68 +28,115 @@ import scala.util.Try
 
 
 @RunWith(classOf[JUnitRunner])
-class StreamingDdlParserSpec extends BaseXDTest with MockitoSugar{
+class StreamingDdlParserSpec extends BaseXDTest with StreamingDDLTestConstants with MockitoSugar {
 
-    val xdContext = mock[XDContext]
-    val parser = new XDDdlParser(_ => null, xdContext)
-
-  override val catalogConfig : Option[Config] =
-    Try(ConfigFactory.load("core-catalog.conf").getConfig(CoreConfig.ParentConfigName)).toOption
+  val xdContext = mock[XDContext]
+  val parser = new XDDdlParser(_ => null, xdContext)
 
 
-  DescribeEphemeralTable
-  ShowEphemeralTables
-  CreateEphemeralTable
-  DropEphemeralTable
-  DropAllEphemeralTables
-  ShowEphemeralStatus
-  ShowAllEphemeralStatuses
-  ShowEphemeralQueries
-  AddEphemeralQuery
-  DropEphemeralQuery
-  DropAllEphemeralQueries
-  StartProcess
-  StopProcess
-
+  // EPHEMERAL TABLE
   "StreamingDDLParser" should "parse a create ephemeral table" in {
-
-    val sqlContext = _xdContext
-
-    val temp = sqlContext.ddlParser.parse("ADD SELECT * FROM t")
-
-    temp.toString()
+    val logicalPlan =
+      parser.parse(s"CREATE EPHEMERAL TABLE $EphemeralTableName (id STRING) OPTIONS (kafka.options.opKey 'value')")
+    logicalPlan shouldBe CreateEphemeralTable(EphemeralTableIdentifier, Some(EphemeralTableSchema),  Map("kafka.options.opKey" -> "value"))
   }
 
-/*  val create = sql("CREATE EPHEMERAL TABLE ephemeralTest1 (id STRING) OPTIONS(kafka.options.test 'optionalConfig')").collect()
-  val getStatus = sql("GET EPHEMERAL STATUS ephemeralTest1").collect()
-  val getAllStatus = sql("GET EPHEMERAL STATUSES").collect()
-  val getTable = sql("DESCRIBE EPHEMERAL TABLE ephemeralTest1").collect()
-  val getAllTables = sql("SHOW EPHEMERAL TABLES").collect()
-  val dropTable = sql("DROP EPHEMERAL TABLE ephemeralTest1").collect()
-  sql("CREATE EPHEMERAL TABLE ephemeralTest1 (id STRING) OPTIONS(kafka.options.test 'optionalConfig')").collect()
-  sql("CREATE EPHEMERAL TABLE ephemeralTest2 (id STRING) OPTIONS(kafka.options.test 'optionalConfig')").collect()
-  val dropAllTables = sql("DROP EPHEMERAL TABLES").collect()
-  */
+  it should "parse a create ephemeral table without schema" in {
+    val logicalPlan =
+      parser.parse(s"CREATE EPHEMERAL TABLE $EphemeralTableName OPTIONS (kafka.options.opKey 'value')")
+    logicalPlan shouldBe CreateEphemeralTable(EphemeralTableIdentifier, None,  Map("kafka.options.opKey" -> "value"))
+  }
+
+  it should "parse a describe ephemeral table" in {
+    val logicalPlan = parser.parse(s"DESCRIBE EPHEMERAL TABLE $EphemeralTableName")
+    logicalPlan shouldBe DescribeEphemeralTable(EphemeralTableIdentifier)
+  }
+
+  it should "parse a show ephemeral tables" in {
+    val logicalPlan = parser.parse(s"SHOW EPHEMERAL TABLES")
+    logicalPlan shouldBe ShowEphemeralTables
+  }
+
+  it should "parse a drop ephemeral table" in {
+    val logicalPlan = parser.parse(s"DROP EPHEMERAL TABLE $EphemeralTableName")
+    logicalPlan shouldBe DropEphemeralTable(EphemeralTableIdentifier)
+  }
+
+  it should "parse a drop all ephemeral tables" in {
+    val logicalPlan = parser.parse(s"DROP ALL EPHEMERAL TABLES")
+    logicalPlan shouldBe DropAllEphemeralTables
+  }
 
 
-/*
- it should "parse... 2" in {
+  // STATUS
+  it should "parse a show ephemeral status" in {
+    val logicalPlan = parser.parse(s"SHOW EPHEMERAL STATUS IN $EphemeralTableName")
+    logicalPlan shouldBe ShowEphemeralStatus(EphemeralTableIdentifier)
+  }
+  it should "parse a show all ephemeral statuses" in {
+    val logicalPlan = parser.parse(s"SHOW EPHEMERAL STATUSES")
+    logicalPlan shouldBe ShowAllEphemeralStatuses
+  }
 
-    val sqlContext = _xdContext
-    sqlContext.sql("CREATE EPHEMERAL TABLE t OPTIONS (receiver.kafka.topic 'ephtable:1', receiver.kafka.groupId 'xd1')")
-   // val temp = sqlContext.ddlParser.parse("ADD SELECT * FROM t WITH WINDOW 5 SECS AS topic")
-    sqlContext.sql("ADD SELECT count(*) FROM t WITH WINDOW 5 SECS AS qalias")
-    //temp.toString()
-  }*/
-
-  it should "parse... 2" in {
-
-    val sqlContext = _xdContext
-    sqlContext.sql("START t")
-    Thread.sleep(50000)
-    sqlContext.sql("STOP t")
+  // START AND STOP PROCESS
+  it should "parse a start process" in {
+    val logicalPlan = parser.parse(s"START $EphemeralTableName")
+    logicalPlan shouldBe StartProcess(EphemeralTableIdentifier.unquotedString)
 
   }
+  it should "parse a stop process" in {
+    val logicalPlan = parser.parse(s"STOP $EphemeralTableName")
+    logicalPlan shouldBe StopProcess(EphemeralTableIdentifier.unquotedString)
+  }
+
+  // QUERIES
+  it should "parse a show ephemeral queries" in {
+    val logicalPlan = parser.parse(s"SHOW EPHEMERAL QUERIES")
+    logicalPlan shouldBe ShowEphemeralQueries(None)
+  }
+
+
+  it should "parse a show ephemeral queries with a specific table" in {
+    val logicalPlan = parser.parse(s"SHOW EPHEMERAL QUERIES IN $EphemeralTableName")
+    logicalPlan shouldBe ShowEphemeralQueries(Some(EphemeralTableIdentifier.unquotedString))
+  }
+
+  it should "fail parsing an add query without window" in {
+    an [Exception] should be thrownBy parser.parse(s"ADD $Sql AS topic")
+  }
+
+  it should "parse a drop  ephemeral query" in {
+    val logicalPlan = parser.parse(s"DROP EPHEMERAL QUERY $QueryName")
+    logicalPlan shouldBe DropEphemeralQuery(QueryName)
+
+  }
+
+
+  it should "parse a drop all ephemeral queries" in {
+    val logicalPlan = parser.parse(s"DROP ALL EPHEMERAL QUERIES")
+    DropAllEphemeralQueries()
+
+  }
+
+  it should "parse a drop all ephemeral queries in a specific table" in {
+    val logicalPlan = parser.parse(s"DROP ALL EPHEMERAL QUERIES IN $EphemeralTableName")
+    DropAllEphemeralQueries(Some(EphemeralTableName))
+
+  }
+
 
 }
 
+trait StreamingDDLTestConstants {
+  // Ephemeral table
+  val EphemeralTableName = "epheTab"
+  val EphemeralTableIdentifier = TableIdentifier(EphemeralTableName)
+  val EphemeralTableSchema = StructType(Seq(StructField("id", StringType)))
+  val QueryName = "qName"
+  val Sql = s"SELECT * FROM $EphemeralTableName"
+  val Window = 5
+
+  val ZookeeperStreamingConnectionKey = "streaming.catalog.zookeeper.connectionString"
+  val ZookeeperConnection: Option[String] =
+    Try(ConfigFactory.load().getString(ZookeeperStreamingConnectionKey)).toOption
+}
