60f6990852283fe9d98b57a10cd992e17929dc79
Hugo Dominguez <hdominguez@stratio.com>
2016-04-19 11:57:23 +0200

    [CROSSDATA-379][testAT]Acceptance tests for DropViews and DropTables

--- testsAT/src/test/java/com/stratio/tests/ATElasticSearchXDTest.java
+++ testsAT/src/test/java/com/stratio/tests/ATElasticSearchXDTest.java
@@ -48,7 +48,8 @@ import com.stratio.tests.utils.ThreadProperty;
 import cucumber.api.CucumberOptions;
 
 //Indicar feature
-@CucumberOptions(features = { "src/test/resources/features/Elasticsearch/ElasticSearchSelectSimple.feature",
+@CucumberOptions(features = {
+        "src/test/resources/features/Elasticsearch/ElasticSearchSelectSimple.feature",
           "src/test/resources/features/Elasticsearch/ElasticSearchelectAnd.feature",
           "src/test/resources/features/Elasticsearch/ElasticSearchSelectINFilter.feature",
           "src/test/resources/features/Elasticsearch/ElasticSearchSelectEqualsFilter.feature",
@@ -58,7 +59,9 @@ import cucumber.api.CucumberOptions;
           "src/test/resources/features/Elasticsearch/ElasticSearchSelectLessEqualsFilter.feature",
         "src/test/resources/features/Udaf/Group_concat.feature",
         "src/test/resources/features/Elasticsearch/TemporaryViews.feature",
-        "src/test/resources/features/Elasticsearch/Views.feature"
+        "src/test/resources/features/Elasticsearch/Views.feature",
+        "src/test/resources/features/Views/DropViews.feature"
+
 })
 public class ATElasticSearchXDTest extends BaseTest {
     private String elasticSearchCluster = System.getProperty("ES_CLUSTER", "elasticsearch");
