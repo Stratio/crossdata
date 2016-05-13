0091c25d4439e4ae20b1237fd5aed59b98420da2
David Arroyo Cazorla <darroyo@stratio.com>
2016-05-13 13:53:29 +0200

    don't add persisted schema when loading tables using the jdbc datasource (#540)

--- core/src/main/scala/org/apache/spark/sql/crossdata/catalog/XDCatalog.scala
+++ core/src/main/scala/org/apache/spark/sql/crossdata/catalog/XDCatalog.scala
@@ -16,7 +16,7 @@
 package org.apache.spark.sql.crossdata.catalog
 
 import org.apache.spark.sql.catalyst.analysis.{Catalog, UnresolvedRelation}
-import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, Subquery}
+import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, Project, Subquery}
 import org.apache.spark.sql.catalyst.{CatalystConf, SimpleCatalystConf, TableIdentifier, util}
 import org.apache.spark.sql.crossdata.catalog.XDCatalog.{CrossdataTable, ViewIdentifier}
 import org.apache.spark.sql.crossdata.execution.datasources.StreamingRelation
@@ -171,6 +171,9 @@ abstract class XDCatalog(val conf: CatalystConf = new SimpleCatalystConf(true),
         val msg = s"Unexpected datasource: $other"
         logError(msg)
         throw new RuntimeException(msg)
+    }
+
+    val resolved = ResolvedDataSource(xdContext, tableSchema, crossdataTable.partitionColumn, crossdataTable.datasource, crossdataTable.opts)
     }
 
     val resolved = ResolvedDataSource(xdContext, tableSchema, crossdataTable.partitionColumn, crossdataTable.datasource, crossdataTable.opts)
