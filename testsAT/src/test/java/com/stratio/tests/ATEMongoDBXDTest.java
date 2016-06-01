4819ec98ecb1559d876e7350c8e3e610b0eb8b3c
Hugo Domínguez Sanz <hdominguez@stratio.com>
2016-06-01 14:45:34 +0200

    Fixed error in ATCassandraXDTest (#561)
    * [CROSSDATA-379][testAT]Acceptance tests for DropViews and DropTables
    
    * [testsAT]Fixed ES acceptance tests
    
    * Upgrade Stratio Parent version
    
    * Removed documentation module
    
    * Rename deb/rpm
    
    * Rename deb/rpm
    
    * Adapted CrossBuild to packages names
    
    * Removed maven-antrun-plugin
    
    * Tests refactor
    
    * Little testAT refactor
    
    * Adapted to Crossbuild
    
    * Fixed errors
    
    * Fixed licenses
    
    * Fixed error in ATCassandraXDTest
    
    * Cassandra tests only

--- testsAT/src/test/java/com/stratio/tests/ATEMongoDBXDTest.java
+++ testsAT/src/test/java/com/stratio/tests/ATEMongoDBXDTest.java
@@ -130,7 +130,7 @@ public class ATEMongoDBXDTest extends BaseTest{
         mongoClient.dropDatabase(dataBase);
     }
 
-    @Test(enabled = true, groups = {"basic"})
+    @Test(enabled = true, groups = {"advanced"})
     public void ATMongoDBXDTest() throws Exception{
         new CucumberRunner(this.getClass()).runCukes();
     }
