8c1b0063b1f6fb838e6ef1f7e1b74bde785f8e71
darroyocazorla <darroyo@stratio.com>
2016-06-20 16:42:15 +0200

    test rich table identifier

---
+++ core/src/test/scala/org/apache/spark/sql/crossdata/catalog/XDCatalogCommonSpec.scala
@@ -0,0 +1,24 @@
+package org.apache.spark.sql.crossdata.catalog
+
+import com.stratio.crossdata.test.BaseXDTest
+import org.apache.spark.sql.catalyst.{SimpleCatalystConf, TableIdentifier}
+import org.apache.spark.sql.crossdata.catalog.interfaces.XDCatalogCommon
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class XDCatalogCommonSpec extends BaseXDTest {
+
+  "A Rich table identifier" should "normalize a table identifier based on catalyst conf" in {
+    val tableName = "myTable"
+    val dbName = "myDB"
+    val tableIdentifier = TableIdentifier(tableName, Some(dbName))
+    import XDCatalogCommon._
+
+    tableIdentifier.normalize(new SimpleCatalystConf(true)) shouldBe TableIdentifier(tableName, Some(dbName))
+
+    tableIdentifier.normalize(new SimpleCatalystConf(false)) shouldBe TableIdentifier(tableName.toLowerCase, Some(dbName.toLowerCase))
+
+  }
+}
