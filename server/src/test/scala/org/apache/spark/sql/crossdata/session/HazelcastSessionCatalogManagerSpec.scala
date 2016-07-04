3a38be4447f32da73003872d0a9f650784516f62
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2016-07-04 12:58:18 +0200

    this test relied on the the -proven false- premise that different hazelcast instances on the same jvm would be taggered as different cluster members.

---
+++ server/src/test/scala/org/apache/spark/sql/crossdata/session/HazelcastSessionCatalogManagerSpec.scala
@@ -0,0 +1,86 @@
+package org.apache.spark.sql.crossdata.session
+
+import java.util.UUID
+
+import akka.actor.{ActorRef, ActorSystem}
+import akka.testkit.{ImplicitSender, TestKit}
+import com.hazelcast.config.{GroupConfig, XmlConfigBuilder}
+import com.hazelcast.core.{Hazelcast, HazelcastInstance}
+import org.apache.spark.sql.catalyst.EmptyConf
+import org.apache.spark.sql.crossdata.XDSessionProvider.SessionID
+import org.apache.spark.sql.crossdata.session.HazelcastSessionCatalogManagerSpec.{InvalidatedSession, ProbedHazelcastSessionCatalogManager}
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+object HazelcastSessionCatalogManagerSpec {
+
+  private class ProbedHazelcastSessionCatalogManager(hInstance: HazelcastInstance)(implicit monitorActor: ActorRef)
+    extends HazelcastSessionCatalogManager(hInstance, EmptyConf) {
+
+    override def invalidateLocalCaches(key: SessionID): Unit = {
+      super.invalidateLocalCaches(key)
+      monitorActor ! InvalidatedSession(key)
+    }
+
+    override def invalidateAllLocalCaches: Unit = {
+      super.invalidateAllLocalCaches
+      monitorActor ! InvalidatedAllSessions
+    }
+
+  }
+
+  case class InvalidatedSession(sessionID: SessionID)
+  case object InvalidatedAllSessions
+
+}
+
+class HazelcastSessionCatalogManagerSpec extends TestKit(ActorSystem("HZSessionCatalogTest"))
+  with WordSpecLike
+  with BeforeAndAfterAll
+  with ImplicitSender {
+
+  // Test description
+
+  "HazelcastSessionCatalogManager local cache" when {
+
+    val sessionID: SessionID = UUID.randomUUID
+
+    "a catalog change at other peer has been performed" should {
+
+      "be invalidated if the change consist on adding or removing new catalogs" in {
+
+        catalogManager.newResource(sessionID)
+        expectMsg(InvalidatedSession(sessionID))
+
+        catalogManager.deleteSessionResource(sessionID)
+        expectMsg(InvalidatedSession(sessionID))
+
+      }
+
+
+    }
+
+  }
+
+  // Test plumbing
+
+  private def createHazelcastInstance: HazelcastInstance = {
+    val xmlConfig = new XmlConfigBuilder().build()
+    xmlConfig.setGroupConfig(new GroupConfig("test"))
+    Hazelcast.newHazelcastInstance(xmlConfig)
+  }
+
+  var catalogManager: HazelcastSessionCatalogManager = _
+  var probedCatalogManager: HazelcastSessionCatalogManager = _
+
+  override protected def beforeAll(): Unit = {
+    val hInstanceA = createHazelcastInstance
+    val hInstanceB = createHazelcastInstance
+
+    catalogManager = new HazelcastSessionCatalogManager(hInstanceA, EmptyConf)
+    probedCatalogManager = new ProbedHazelcastSessionCatalogManager(hInstanceB)
+
+  }
+
+  override protected def afterAll(): Unit = Hazelcast shutdownAll
+
+}
