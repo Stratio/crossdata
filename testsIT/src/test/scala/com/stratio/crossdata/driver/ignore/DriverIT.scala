c930832b43f99a3ceb376a72da64b5ce173d13f3
darroyocazorla <darroyo@stratio.com>
2016-07-18 11:45:05 +0200

    [test] allow running multiple drivers per JVM

--- testsIT/src/test/scala/com/stratio/crossdata/driver/ignore/DriverIT.scala
+++ testsIT/src/test/scala/com/stratio/crossdata/driver/ignore/DriverIT.scala
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
 import scala.language.postfixOps
+import scala.util.Random
 
 @RunWith(classOf[JUnitRunner])
 class DriverIT extends BaseXDTest {
@@ -40,6 +41,34 @@ class DriverIT extends BaseXDTest {
         validateResult(result)
       }
 
+    }
+  }
+
+  it should "be able to specify custom spark sql parameters" ignore { // TODO make a test
+
+    val randomTable = s"random${Math.abs(Random.nextLong())}"
+
+    withDriverDo { driver =>
+      withDriverDo { anotherDriver =>
+
+        driver.sql(s"CREATE TEMPORARY TABLE $randomTable USING org.apache.spark.sql.json OPTIONS (path '${Paths.get(getClass.getResource("/tabletest.json").toURI).toString}')").waitForResult()
+        anotherDriver.sql(s"CREATE TEMPORARY TABLE $randomTable USING org.apache.spark.sql.json OPTIONS (path '${Paths.get(getClass.getResource("/tabletest.json").toURI).toString}')").waitForResult()
+
+        driver.sql(s"SET spark.sql.shuffle.partitions=400").waitForResult()
+        anotherDriver.sql(s"SET spark.sql.shuffle.partitions=400").waitForResult()
+
+        Thread.sleep(100)
+
+        for (_ <- 1 to 3) {
+          // It assumes that the driver has a round robin policy
+          val result = driver.sql(s"SELECT title, count(*) FROM $randomTable GROUP BY title").waitForResult()
+          validateResult(result)
+
+          val result2 = anotherDriver.sql(s"SELECT title, count(*) FROM $randomTable GROUP BY title").waitForResult()
+          validateResult(result2)
+        }
+
+      }
     }
   }
 
