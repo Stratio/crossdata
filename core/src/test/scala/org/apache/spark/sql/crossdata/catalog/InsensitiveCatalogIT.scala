23084a76510cf967f57b767199c941d3a6db06e2
Unai Sarasola <usarasola@stratio.com>
2016-07-20 10:55:43 +0200

    Overrided case sensitive

--- core/src/test/scala/org/apache/spark/sql/crossdata/catalog/InsensitiveCatalogIT.scala
+++ core/src/test/scala/org/apache/spark/sql/crossdata/catalog/InsensitiveCatalogIT.scala
@@ -17,9 +17,11 @@ package org.apache.spark.sql.crossdata.catalog
 
 import com.typesafe.config.{Config, ConfigFactory, ConfigValueFactory}
 import org.apache.spark.sql.catalyst.TableIdentifier
+import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, OneRowRelation}
 import org.apache.spark.sql.crossdata.catalog.XDCatalog.CrossdataTable
 import org.apache.spark.sql.crossdata.catalog.interfaces.XDCatalogCommon
 import org.apache.spark.sql.crossdata.catalog.persistent.DerbyCatalogIT
+import org.apache.spark.sql.execution.datasources.LogicalRelation
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
@@ -28,7 +30,7 @@ import org.scalatest.junit.JUnitRunner
 class InsensitiveCatalogIT extends DerbyCatalogIT {
 
   override val catalogConfig : Option[Config] =
-    Some(ConfigFactory.empty().withValue("crossdata-core.catalog.caseSensitive", ConfigValueFactory.fromAnyRef(false)))
+    Some(ConfigFactory.empty().withValue("catalog.caseSensitive", ConfigValueFactory.fromAnyRef(false)))
 
 
   it should s"persist a table and retrieve it changing some letters to upper case in $catalogName" in {
@@ -36,10 +38,10 @@ class InsensitiveCatalogIT extends DerbyCatalogIT {
     val tableNameOriginal = "TableNameInsensitive"
     import XDCatalogCommon._
     val tableIdentifier = TableIdentifier(tableNameOriginal, Some(Database))
-    val tableNormalized = tableIdentifier.normalize(xdContext.catalog.conf)
+    val tableNormalized = tableIdentifier.normalize
     val crossdataTable = CrossdataTable(tableNormalized, Some(Columns), SourceDatasource, Array[String](Field1Name), OptsJSON)
 
-    xdContext.catalog.persistTableMetadata(crossdataTable)
+    xdContext.catalog.persistTable(crossdataTable, OneRowRelation)
     xdContext.catalog.tableExists(tableIdentifier) shouldBe true
 
     val tableNameOriginal2 = "tablenameinsensitive"
