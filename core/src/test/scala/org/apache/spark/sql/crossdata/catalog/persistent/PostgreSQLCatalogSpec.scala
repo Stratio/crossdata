606a4dedf24c87446d599e8464ff1d1a186a0b01
Unai Sarasola <pianista215@users.noreply.github.com>
2016-08-25 09:23:00 +0200

    [CROSSDATA-552] Prefix catalogs and Zookeeper configurable paths (#632)
    * Solved bug with catalog speconfig for Index metadata table in MySql and Postgres
    
    * Removed ids
    
    * Bugs solved with the name of the catalogs and added as properties to the core-reference.conf
    
    * Removed table configurations replaced by clustername
    
    * MySQL catalog tests
    
    * Postgres unitary test
    
    * MySQL and PostgreSQL changed visibility from protected[crossdata] to protected
    
    * Solved and redone the tests
    
    * Changed core-references adding prefix
    
    * Solved tests
    
    * Solved problems with prefix in EphemeralQueryActor
    
    * Solved tests
    
    * Resolved compile failure
    
    * Changed to prefix
    
    * Removed author scaladoc
    
    * Reformatting
    
    * MySQL changed format of properties
    
    * Changed in zookeeper as suggested by @darroyo
    
    * With .zookeeper.
    
    * Constants for streaming
    
    * Changed with new folders
    
    * Added to the IT the prefix as @darroyo suggested
    
    * Added to streaming
    
    * Solved problems with Kafka

---
+++ core/src/test/scala/org/apache/spark/sql/crossdata/catalog/persistent/PostgreSQLCatalogSpec.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.sql.crossdata.catalog.persistent
+
+import com.stratio.crossdata.test.BaseXDTest
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.sql.catalyst.{CatalystConf, SimpleCatalystConf}
+import org.apache.spark.sql.crossdata.config.CoreConfig
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class PostgreSQLCatalogSpec extends BaseXDTest {
+
+  private class PostgreSQLCatalogPublicMetadata(override val catalystConf: CatalystConf) extends PostgreSQLXDCatalog(catalystConf){
+    val tablesPrefixTest = tablesPrefix
+    val tableWithTableMetadataTest = tableWithTableMetadata
+    val tableWithViewMetadataTest = tableWithViewMetadata
+    val tableWithAppJarsTest = tableWithAppJars
+    val tableWithIndexMetadataTest = tableWithIndexMetadata
+    val configTest = config
+  }
+
+  private class PostgreSQLCatalogWithMockedConfig(override val catalystConf: CatalystConf) extends PostgreSQLCatalogPublicMetadata(catalystConf) {
+    override lazy val config: Config =
+      ConfigFactory.load("catalogspec/postgresql-catalog-test-properties.conf").getConfig(Seq(CoreConfig.ParentConfigName, CoreConfig.CatalogConfigKey) mkString ".")
+  }
+
+  it should "get the cluster name from the config if specified" in {
+    val catalog = new PostgreSQLCatalogWithMockedConfig(new SimpleCatalystConf(true))
+    catalog.configTest.getString("prefix") shouldBe "crossdataPostgresClusterTest"
+    catalog.tablesPrefixTest shouldBe "crossdataPostgresClusterTest_"
+    catalog.tableWithTableMetadataTest shouldBe "crossdataPostgresClusterTest_crossdataTables"
+    catalog.tableWithViewMetadataTest shouldBe "crossdataPostgresClusterTest_crossdataViews"
+    catalog.tableWithAppJarsTest shouldBe "crossdataPostgresClusterTest_crossdataJars"
+    catalog.tableWithIndexMetadataTest shouldBe "crossdataPostgresClusterTest_crossdataIndexes"
+  }
+
+  it should "work with the default values if cluster name is not specified" in {
+    val catalog = new PostgreSQLCatalogPublicMetadata(new SimpleCatalystConf(true))
+    an[Exception] shouldBe thrownBy(catalog.configTest.getString("prefix"))
+    catalog.tablesPrefixTest shouldBe ""
+    catalog.tableWithTableMetadataTest shouldBe "crossdataTables"
+    catalog.tableWithViewMetadataTest shouldBe "crossdataViews"
+    catalog.tableWithAppJarsTest shouldBe "crossdataJars"
+    catalog.tableWithIndexMetadataTest shouldBe "crossdataIndexes"
+  }
+
+}
