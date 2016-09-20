cae7b8b5a4a7284a9f9012e97a88097e4eec741a
David Arroyo Cazorla <darroyo@stratio.com>
2016-09-20 13:05:03 +0200

    [CROSSDATA-659] Fix spark UDFs execution involving 'NativeFunctionExecutors' connectors (#694)

--- core/src/main/scala/org/apache/spark/sql/crossdata/catalyst/planning/patterns.scala
+++ core/src/main/scala/org/apache/spark/sql/crossdata/catalyst/planning/patterns.scala
@@ -15,7 +15,7 @@
  */
 package org.apache.spark.sql.crossdata.catalyst.planning
 
-//import org.apache.spark.sql.catalyst.expressions._
+import com.stratio.crossdata.connector.NativeFunctionExecutor
 import org.apache.spark.sql.catalyst.expressions.PredicateHelper
 import org.apache.spark.sql.catalyst.expressions.NamedExpression
 import org.apache.spark.sql.catalyst.expressions.Expression
@@ -27,25 +27,38 @@ import org.apache.spark.sql.catalyst.plans.logical.Filter
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.plans.logical.Project
 import org.apache.spark.sql.crossdata.catalyst.EvaluateNativeUDF
+import org.apache.spark.sql.execution.datasources.LogicalRelation
+import org.apache.spark.sql.sources.CatalystToCrossdataAdapter
+import org.apache.spark.sql.sources.CatalystToCrossdataAdapter.CrossdataExecutionPlan
 
-
+/**
+  * WARNING: The extended physical operation only match when the base relation is an instance of [[NativeFunctionExecutor]]
+  * Otherwise, delegate to the next strategy
+  */
 object ExtendedPhysicalOperation extends PredicateHelper {
-  type ReturnType = (Seq[NamedExpression], Seq[Expression], LogicalPlan)
+  type ReturnType = (Seq[NamedExpression], Seq[Expression], LogicalPlan, CrossdataExecutionPlan)
 
   def unapply(plan: LogicalPlan): Option[ReturnType] = {
     val (fields, filters, child, _) = collectProjectsAndFilters(plan)
-    Some((fields.getOrElse(child.output), filters, child))
+    val projects = fields.getOrElse(child.output)
+
+    child match {
+      case LogicalRelation(t: NativeFunctionExecutor, _) =>
+        Some((projects, filters, child, CatalystToCrossdataAdapter.getConnectorLogicalPlan(plan, projects, filters))) // TODO it does not apply
+      case _ =>
+        None
+    }
   }
 
   def collectProjectsAndFilters(plan: LogicalPlan):
   (Option[Seq[NamedExpression]], Seq[Expression], LogicalPlan, Map[Attribute, Expression]) =
     plan match {
-      case Project(fields, child) =>
+      case Project(fields, child) if fields.forall(_.deterministic) =>
         val (_, filters, other, aliases) = collectProjectsAndFilters(child)
         val substitutedFields = fields.map(substitute(aliases)).asInstanceOf[Seq[NamedExpression]]
         (Some(substitutedFields), filters, other, collectAliases(substitutedFields))
 
-      case Filter(condition, child) =>
+      case Filter(condition, child) if condition.deterministic =>
         val (fields, filters, other, aliases) = collectProjectsAndFilters(child)
         val substitutedCondition = substitute(aliases)(condition)
         (fields, filters ++ splitConjunctivePredicates(substitutedCondition), other, aliases)
