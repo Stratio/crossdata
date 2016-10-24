7b08327e956dda71223d6684932c73fa4d07ff9d
pmadrigal <pedromadri89@hotmail.com>
2016-10-24 22:15:56 +0200

    method created to convert SQL type to sparkSQL type

--- postgresql/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/PostgresqlUtils.scala
+++ postgresql/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/PostgresqlUtils.scala
@@ -16,7 +16,7 @@
 
 package org.apache.spark.sql.execution.datasources.jdbc
 
-import java.sql.{Connection, Statement}
+import java.sql.{Connection, SQLException, Statement}
 import java.util.Properties
 
 import org.apache.spark.Partition
@@ -31,14 +31,74 @@ object PostgresqlUtils {
   val driverClassName: String = "org.postgresql.Driver"
 
   def withClientDo[T](parameters: Map[String, String])(f: (Connection, Statement) => T): T = {
-    val client = buildConnection(parameters)
-    val statement = client.createStatement()
+    val connection = buildConnection(parameters)
+    val statement = connection.createStatement()
     try {
-      f(client, statement)
+      f(connection, statement)
     } finally {
-      client.close()
+      connection.close()
       statement.close()
     }
+  }
+
+  /**
+    * Maps a JDBC type to a Catalyst type.
+    *
+    * @param sqlType - A field of java.sql.Types
+    * @return The Catalyst type corresponding to sqlType.
+    */
+  def getCatalystType( sqlType: Int,
+                       precision: Int,
+                       scale: Int,
+                       signed: Boolean): DataType = {
+    val answer = sqlType match {
+      // scalastyle:off
+      case java.sql.Types.ARRAY         => null
+      case java.sql.Types.BIGINT        => if (signed) { LongType } else { DecimalType(20,0) }
+      case java.sql.Types.BINARY        => BinaryType
+      case java.sql.Types.BIT           => BooleanType // @see JdbcDialect for quirks
+      case java.sql.Types.BLOB          => BinaryType
+      case java.sql.Types.BOOLEAN       => BooleanType
+      case java.sql.Types.CHAR          => StringType
+      case java.sql.Types.CLOB          => StringType
+      case java.sql.Types.DATALINK      => null
+      case java.sql.Types.DATE          => DateType
+      case java.sql.Types.DECIMAL
+        if precision != 0 || scale != 0 => DecimalType.bounded(precision, scale)
+      case java.sql.Types.DECIMAL       => DecimalType.SYSTEM_DEFAULT
+      case java.sql.Types.DISTINCT      => null
+      case java.sql.Types.DOUBLE        => DoubleType
+      case java.sql.Types.FLOAT         => FloatType
+      case java.sql.Types.INTEGER       => if (signed) { IntegerType } else { LongType }
+      case java.sql.Types.JAVA_OBJECT   => null
+      case java.sql.Types.LONGNVARCHAR  => StringType
+      case java.sql.Types.LONGVARBINARY => BinaryType
+      case java.sql.Types.LONGVARCHAR   => StringType
+      case java.sql.Types.NCHAR         => StringType
+      case java.sql.Types.NCLOB         => StringType
+      case java.sql.Types.NULL          => null
+      case java.sql.Types.NUMERIC
+        if precision != 0 || scale != 0 => DecimalType.bounded(precision, scale)
+      case java.sql.Types.NUMERIC       => DecimalType.SYSTEM_DEFAULT
+      case java.sql.Types.NVARCHAR      => StringType
+      case java.sql.Types.OTHER         => null
+      case java.sql.Types.REAL          => DoubleType
+      case java.sql.Types.REF           => StringType
+      case java.sql.Types.ROWID         => LongType
+      case java.sql.Types.SMALLINT      => IntegerType
+      case java.sql.Types.SQLXML        => StringType
+      case java.sql.Types.STRUCT        => StringType
+      case java.sql.Types.TIME          => TimestampType
+      case java.sql.Types.TIMESTAMP     => TimestampType
+      case java.sql.Types.TINYINT       => IntegerType
+      case java.sql.Types.VARBINARY     => BinaryType
+      case java.sql.Types.VARCHAR       => StringType
+      case _                            => null
+      // scalastyle:on
+    }
+
+    if (answer == null) throw new SQLException("Unsupported type " + sqlType)
+    answer
   }
 
   private def buildConnection(parameters: Map[String, String]): Connection = {
