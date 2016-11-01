d34c0a7d0c433efcc8525b734d27e5ccb78ebb09
pmadrigal <pedromadri89@hotmail.com>
2016-11-01 18:36:29 +0100

    changes in PostgresqlSortIT

--- postgresql/src/test/scala/com/stratio/crossdata/connector/postgresql/PostgresqlSortIT.scala
+++ postgresql/src/test/scala/com/stratio/crossdata/connector/postgresql/PostgresqlSortIT.scala
@@ -6,12 +6,21 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class PostgresqlSortIT extends PostgresqlWithSharedContext{
 
-  "The Postgresql connector" should s"support a (SELECT * .. ORDER BY ) natively" in {
+  "The Postgresql connector" should s"support a (SELECT * .. ORDER BY DESC ) natively" in {
     assumeEnvironmentIsUpAndRunning
 
-    val df = sql(s"SELECT * FROM $Table ORDER BY id ASC, comment DESC")
-      println(df.queryExecution.toString())
-      df.collect() should have length 0
+    val df = sql(s"SELECT * FROM $Table ORDER BY id DESC")
+    val result = df.collect()
+    result(0).getInt(0) should be (10)
+
   }
 
+  it should s"support a (SELECT * .. ORDER BY ASC) natively" in {
+    assumeEnvironmentIsUpAndRunning
+
+    val df = sql(s"SELECT * FROM $Table ORDER BY id ASC")
+    val result = df.collect()
+    result(0).getInt(0) should be (1)
+
+  }
 }
