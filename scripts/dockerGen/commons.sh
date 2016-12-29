8e1011e8be65f6e7098e182e1515f0090de88fa1
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2016-12-29 16:13:57 +0100

    [CROSSDATA] [DOCKER] Script for building docker locally (#80)

--- scripts/dockerGen/commons.sh
+++ scripts/dockerGen/commons.sh
@@ -6,7 +6,7 @@ function changedir() {
 }
 
 #Get Crossdata version from parent pom
-tmp1=`grep -m2 "<version>" ../pom.xml | tail -n1`
+tmp1=`grep -m2 "<version>" ../../pom.xml | tail -n1`
 tmp2=${tmp1/<version>/}
 tmp3=${tmp2/<\/version>/}
 XD_VERSION=${tmp3// }
