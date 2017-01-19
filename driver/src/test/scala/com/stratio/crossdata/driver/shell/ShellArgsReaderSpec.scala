45cd40c31143e0a772d56c13e9c76c67084832d7
Pedro Madrigal Marina <pmadrigal@users.noreply.github.com>
2017-01-19 12:31:58 +0100

    Executing query from the shell (#800) (#807)
    * Executing query from the shell
    
    * --tcp and finish console
    
    * Tcp option in usage
    
    * Implementation with callbacks
    
    * New version with suggestions from pfcoperez
    
    * Refactor.. But is not waiting
    
    * More functional way
    
    * Moved flush
    
    * fix race conditions when executing sync queries (#2)
    
    * Suggestions by ma

--- driver/src/test/scala/com/stratio/crossdata/driver/shell/ShellArgsReaderSpec.scala
+++ driver/src/test/scala/com/stratio/crossdata/driver/shell/ShellArgsReaderSpec.scala
@@ -23,23 +23,28 @@ import org.scalatest.junit.JUnitRunner
 class ShellArgsReaderSpec extends BaseXDTest {
 
   "ShellArgsReader" should "parse boolean options adding a flag" in {
-    val opts = new ShellArgsReader(List("--http")).options
+    val opts = new ShellArgsReader(List("--tcp")).options
 
-    opts("http") shouldBe true
+    opts("tcp") shouldBe true
     opts.get("async") shouldBe empty
 
-    val options = new ShellArgsReader(List("--http", "--async")).options
-    options("http") shouldBe true
+    val options = new ShellArgsReader(List("--tcp", "--async")).options
+    options("tcp") shouldBe true
     options("async") shouldBe true
 
   }
 
-  it should "parse boolean options indicating the value" in {
-    val opts = new ShellArgsReader(List("--http", "true")).options
-    opts("http") shouldBe true
+  it should "parse a query passed as parameter" in {
+    val opts = new ShellArgsReader(List("--query", "show tables")).options
+    opts("query") shouldBe "show tables"
+  }
 
-    val options = new ShellArgsReader(List("--http", "false")).options
-    options("http") shouldBe false
+  it should "parse boolean options indicating the value" in {
+    val opts = new ShellArgsReader(List("--tcp", "true")).options
+    opts("tcp") shouldBe true
+
+    val options = new ShellArgsReader(List("--tcp", "false")).options
+    options("tcp") shouldBe false
   }
 
 }
